496cd8ac2df15b51d33a866d499147a49c317330
Lasse Rafn <lasserafn@gmail.com>
2016-01-15 00:07:42 +0100

    Admin changes (navigation)

--- resources/views/admin/navigation.blade.php
+++ resources/views/admin/navigation.blade.php
@@ -3,7 +3,6 @@
 		<div class="container">
 			<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
 				<i class="icon-reorder shaded"></i></a><a class="brand" href="/dashboard">TakeDaily </a>
-
 			<div class="nav-collapse collapse navbar-inverse-collapse">
 				<ul class="nav pull-right">
 					<li class="nav-user dropdown">
--- resources/views/includes/app/nav.blade.php
+++ resources/views/includes/app/nav.blade.php
@@ -5,15 +5,21 @@
 				<li><a href="{{ url('/login') }}">Login</a></li>
 				<li><a href="{{ url('/register') }}">Register</a></li>
 			@else
-				<li><span>{{ Auth::user()->name }}</span>
-					<ul class="dropdown">
-						<li><a href="/account">My account</a></li>
-						<li><a href="/account/transactions">Orders</a></li>
-						<li><a href="/account/settings/basic">Settings</a></li>
-						<li><a href="/account/settings/billing">Billing</a></li>
-						<li><a href="{{ url('/logout') }}">Logout</a></li>
-					</ul>
-				</li>
+				@if(Auth::user()->isUser())
+					<li><span>{{ Auth::user()->name }}</span>
+						<ul class="dropdown">
+							<li><a href="/account">My account</a></li>
+							<li><a href="/account/transactions">Orders</a></li>
+							<li><a href="/account/settings/basic">Settings</a></li>
+							<li><a href="/account/settings/billing">Billing</a></li>
+							<li><a href="{{ url('/logout') }}">Logout</a></li>
+						</ul>
+					</li>
+				@else
+					<li>
+						<a href="/dashboard">To dashboard</a>
+					</li>
+				@endif
 			@endif
 		</ul>
 	</div>
