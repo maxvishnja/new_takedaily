6cb15cfcf9c75aabd0722c5ccbfbc04600248db0
Lasse Rafn <lasserafn@gmail.com>
2016-02-26 14:08:11 +0100

    SlugLibrary fixes

--- app/Apricot/Libraries/SlugLibrary.php
+++ app/Apricot/Libraries/SlugLibrary.php
@@ -5,16 +5,8 @@ class SlugLibrary
 {
 	public static function generate($title = '')
 	{
-		$title = trim($title, ' ');
-		$title = strtolower($title);
-		$title = preg_replace("/(å)/", 'aa', $title);
-		$title = preg_replace("/(ø)/", 'oe', $title);
-		$title = preg_replace("/(æ)/", 'ae', $title);
-		$title = preg_replace("/\s\s+/", ' ', $title);
-		$title = preg_replace("/( )/", '-', $title);
-		$title = preg_replace("/([^a-z0-9-])/", '', $title);
-		$title = preg_replace("/\-\-+/", '-', $title);
-		$title = substr($title, 0, 50);
+		$title = str_slug($title);
+		$title = str_limit($title, 50, '');
 
 		return $title;
 	}
