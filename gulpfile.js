56db6a4d73789f002e674533778d0fa9fcaa6c4e
Lasse Rafn <lasserafn@gmail.com>
2016-02-29 00:52:50 +0100

    Working on sprites and such.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var svgSprite = require('gulp-svg-sprite');
+require('laravel-elixir-spritesmith');
+
 
 /*
  |--------------------------------------------------------------------------
@@ -13,8 +17,36 @@ var elixir = require('laravel-elixir');
 
 //elixir.extend('sourcemaps', false);
 
+
 elixir(function (mix)
 {
+	svgSpriteConfig = {
+		mode: {
+			css: {
+				render: {
+					scss: {
+						dest: '../../resources/assets/sass/_svg-sprites'
+					}
+				},
+				sprite: 'images/svg-sprite.svg'
+			},
+		}
+	};
+	gulp.src('resources/assets/sprites/*.svg')
+		.pipe(svgSprite(svgSpriteConfig))
+		.pipe(gulp.dest('public/'));
+
+	mix.spritesmith('resources/assets/sprites', {
+		retinaSrcFilter: 'resources/assets/sprites/*@2x.png',
+		imgOutput: 'public/images',
+		cssOutput: 'resources/assets/sass/',
+		cssFormat: 'sass',
+		cssName: '_sprites.sass',
+		imgPath: '/images/sprite.png',
+		retinaImgName: 'sprite@2x.png',
+		retinaImgPath: '/images/sprite@2x.png'
+	});
+
 	mix.sass('app.sass', false, {indentedSyntax: true});
 
 	mix.scripts([
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
 	Home
+	<div class="logo-color"></div>
 @endsection
