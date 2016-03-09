3c06987ad05b09c76a3e5102c4b7853309492ee2
Lasse Rafn <lasserafn@gmail.com>
2016-03-09 15:37:16 +0100

    Almost the entire homepage. Fixed female icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 @section('pageClass', 'index')
 
 @section('content')
-
 	<header class="header--landing header--with-large-bg">
 		<div class="container-fluid">
 			<div class="header_top">
@@ -48,35 +47,35 @@
 		</div>
 	</header>
 
-	<main @if(!isset($mainClasses)) class="m-t-50 m-b-50" @else class="{{ $mainClasses }}" @endif>
+	<main>
 		<div class="container">
 			<div class="block block--one text-center">
 				<h2>Sådan virker det</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi dicta ea, earum enim est ex hic
-					iure iusto maxime minus nam pariatur praesentium repellat repellendus sunt temporibus ullam.</p>
+				<p class="sub_header">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi dicta ea,
+					earum enim est ex hic iure iusto, lorem ipsum.</p>
 
 				<div class="row text-center">
-					<div class="col-md-4">
+					<div class="col-md-4 block_item">
 						<span class="icon icon-user-information"></span>
 						<h3>Konsultationen begynder</h3>
 						<p>Fortæl os lidt om din krop, kost og livsstil. Det tager ikke mere end 2 minutter at svare på
 							spørgsmålene.</p>
 					</div>
 
-					<div class="col-md-4">
+					<div class="col-md-4 block_item">
 						<span class="icon icon-box"></span>
 						<h3>Vi skræddersyr dit Take Daily</h3>
 						<p>Vi udvælger dine vitaminer og mineraler i koncentrationer, der passer nøjagtigt til dig.</p>
 					</div>
 
-					<div class="col-md-4">
+					<div class="col-md-4 block_item">
 						<span class="icon icon-truck"></span>
 						<h3>Du modtager din æske</h3>
 						<p>Du får leveret din æske gratis derhjemme eller på kontoret, hvis det passer dig bedre</p>
 					</div>
 				</div>
 
-				<div class="text-center">
+				<div class="text-center m-t-50">
 					<a href="/flow" class="button button--green button--rounded button--large">Få flere detaljer</a>
 				</div>
 			</div>
@@ -84,20 +83,28 @@
 
 		<div class="block block--two">
 			<div class="container">
-				<h2>Lev lidt sundere.<br/>
-					Start indefra med Take Daily.</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<h2>Lev lidt sundere.<br/>
+							Start indefra med Take Daily.</h2>
 
-				<p>
-					At finde de helt rigtige vitaminer og mineraler, som din krop har brug for, kan i sig selv være en
-					udfordring. Hylderne bugner af multivitaminpiller og komplette kosttilskudspakker. Take Daily er
-					diamentrale modsætninger.
-				</p>
-				<p>Vi udvælger og sammensætter omhyggeligt de vitaminer og mineraler, som lige præcis din krop har brug
-					for. Koncentrationerne er individuelle og kommer i din helt egen æske.
-				</p>
+						<p>
+							At finde de helt rigtige vitaminer og mineraler, som din krop har brug for, kan i sig selv
+							være en
+							udfordring. Hylderne bugner af multivitaminpiller og komplette kosttilskudspakker. Take
+							Daily er
+							diamentrale modsætninger.
+						</p>
+						<p>Vi udvælger og sammensætter omhyggeligt de vitaminer og mineraler, som lige præcis din krop
+							har brug
+							for. Koncentrationerne er individuelle og kommer i din helt egen æske.
+						</p>
 
-				<a href="/flow" class="button button--white button--rounded button--large button--text-grey">Start i
-					dag</a>
+						<a href="/flow" class="button button--white button--rounded button--large button--text-grey">Start
+							i
+							dag</a>
+					</div>
+				</div>
 			</div>
 		</div>
 
@@ -106,7 +113,7 @@
 				<div class="row">
 					<div class="col-md-4">
 						<img src="/images/dietist.png" class="img--rounded" alt="Suzan"/>
-						<span>Suzan, diætist</span>
+						<span class="dietist-name">Suzan, diætist</span>
 					</div>
 					<div class="col-md-8">
 						<blockquote>“Prevention is better than cure. Well said and well understood! But there are
@@ -134,7 +141,7 @@
 
 				<p>Videnskaben bag er kompleks, men for dig er det hele enkelt og ligetil.</p>
 
-				<a href="/om" class="anchor anchor--underline">Få mere baggrundsviden</a>
+				<a href="/om" class="anchor anchor--underline anchor--inline m-t-20">Få mere baggrundsviden</a>
 			</div>
 
 			<div class="clear"></div>
@@ -178,7 +185,7 @@
 											for. Koncentrationerne er individuelle og kommer i din helt egen æske.
 										</p>
 
-										<a href="/flow" class="button button--white button--rounded button--large button--text-grey">Start
+										<a href="/flow" class="button button--white button--rounded button--large button--text-grey m-t-20">Start
 											på
 											Take Daily i dag</a>
 									</div>
@@ -197,29 +204,15 @@
 									</div>
 									<div class="col-md-6">
 										<p>
-											At finde de helt rigtige vitaminer og mineraler, som din krop har brug for,
-											kan
-											i
-											sig
-											selv
-											være en
-											udfordring. Hylderne bugner af multivitaminpiller og komplette
-											kosttilskudspakker.
-											Take
-											Daily er
-											diamentrale modsætninger.  
+											At finde de helt rigtige vitaminer og mineraler, som din krop har brug for, kan i sig selv være en udfordring.
+											Hylderne bugner af multivitaminpiller og komplette kosttilskudspakker. Take Daily er diamentrale modsætninger.  
 										</p>
-										<p>Vi udvælger og sammensætter omhyggeligt de vitaminer og mineraler, som lige
-											præcis
-											din
-											krop
-											har brug
-											for. Koncentrationerne er individuelle og kommer i din helt egen æske.
+										<p>Vi udvælger og sammensætter omhyggeligt de vitaminer og mineraler, som lige præcis din krop har brug for.
+											Koncentrationerne er individuelle og kommer i din helt egen æske.
 										</p>
 
-										<a href="/flow" class="button button--white button--rounded button--large button--text-grey">Start
-											på
-											Take Daily i dag</a>
+										<a href="/flow" class="button button--white button--rounded button--large button--text-grey m-t-20">Start på Take Daily
+											i dag</a>
 									</div>
 								</div>
 							</div>
@@ -231,23 +224,33 @@
 
 		<div class="block block--seven text-center">
 			<div class="container">
-				<h2>Online konsultation</h2>
-				<h3>Hvilke vitaminer og mineraler mangler du?</h3>
-				<p>Vores dygtige diætister står bag spørgsmålene, som giver svar på, hvilke vitaminer og mineraler, du
-					skal
-					tage for at få din krop i balance. Der er 39 spørgsmål, og efter 2 minutter har du dit resultat.</p>
+				<div class="row">
+					<div class="col-md-8 col-md-push-2">
+						<h2>Online konsultation</h2>
+						<h3>Hvilke vitaminer og mineraler mangler du?</h3>
+						<p>Vores dygtige diætister står bag spørgsmålene, som giver svar på, hvilke vitaminer og mineraler, du
+							skal
+							tage for at få din krop i balance. Der er 39 spørgsmål, og efter 2 minutter har du dit resultat.</p>
 
-				<a href="/flow" class="button button--green button--rounded button--large">Start på Take Daily i dag</a>
+						<a href="/flow" class="button button--green button--rounded button--large">Start på Take Daily i dag</a>
+					</div>
+				</div>
 			</div>
 		</div>
 
 		<div class="container">
-			<div class="block block--eight">
-				<img src="/images/product_box_large.png" alt="Take Daily boks"/>
-				<h2>Modtag vitaminer hver måned,<br/>
-					det koster kun 149 kr.</h2>
-				<p>Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag
-					typewriter banh mi, squid keffiyeh High.</p>
+			<div class="block block--eight text-center">
+				<div class="row">
+					<div class="col-md-8 col-md-push-2">
+						<img src="/images/product_box_large.png" alt="Take Daily boks"/>
+						<h2>Modtag vitaminer hver måned,<br/>
+							det koster kun 149 kr.</h2>
+						<p>Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag
+							typewriter banh mi, squid keffiyeh High.</p>
+
+						<a href="/flow" class="button button--green button--rounded button--large m-t-30">Start på Take Daily i dag</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</main>
