3c9a374bc0582a36f66ded7a4d37cda49492a948
Lasse Rafn <lasserafn@gmail.com>
2016-04-09 14:14:34 +0200

    Dashboard

--- resources/views/includes/app/nav.blade.php
+++ resources/views/includes/app/nav.blade.php
@@ -5,6 +5,9 @@
 		@if(Auth::guest() || Auth::user()->isUser())
 			<li><a href="/account"><strong>{{ trans('nav.account.profile') }}</strong></a></li>
 		@endif
+		@if( Auth::user() && Auth::user()->isAdmin() )
+			<li><a href="/dashboard"><strong>Dashboard</strong></a></li>
+		@endif
 	</ul>
 
 	<div class="visible-sm visible-xs">
