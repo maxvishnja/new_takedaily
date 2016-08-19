094f21fe65933519b5c77e712dfabe5fbc1cebfd
lasserafn <lasserafn@gmail.com>
2016-08-19 12:28:15 +0200

    New menu items

--- resources/lang/da/nav.php
+++ resources/lang/da/nav.php
@@ -8,7 +8,8 @@ return [
 		'billing'      => 'Fakturering',
 		'logout'       => 'Log ud'
 	],
-	'links'   => '<li><a href="/fra-a-til-zink">Fra A til Zink</a></li>
-				<li><a href="/sadan-virker-det">Sådan virker det</a></li>
-				<li><a href="/test-and-quality">Test &amp; Kvalitet</a></li>'
+	'links'   => '<li><a href="/sadan-virker-det">Sådan virker det</a></li>
+				  <li><a href="/test-and-quality">Test &amp; kvalitet</a></li>
+				  <li class="hidden-md"><a href="/about">Om os</a></li>
+				  <li><a href="/fra-a-til-zink">Fra A-Zink</a></li>'
 ];
