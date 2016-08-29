450c64fededcd370ca7da970f3bfc36247a793b1
lasserafn <lasserafn@gmail.com>
2016-08-29 12:55:05 +0200

    Pick-n-mix redirect to checkout with vitamins and subscription product

--- app/Http/Controllers/PickMixController.php
+++ app/Http/Controllers/PickMixController.php
@@ -28,8 +28,8 @@ class PickMixController extends Controller
 		$this->validate($request, [
 			'vitamins' => 'min:3|max:4|exists:vitamins,id'
 		], [
-			'vitamins.min' => 'Du har ikke valgt nok vitaminer, du skal mindst vælge 3 forskellige.', // todo translate
-			'vitamins.max' => 'Du har valgt for mange vitaminer, du kan maksimalt vælge 4 forskellige.', // todo translate
+			'vitamins.min'    => 'Du har ikke valgt nok vitaminer, du skal mindst vælge 3 forskellige.', // todo translate
+			'vitamins.max'    => 'Du har valgt for mange vitaminer, du kan maksimalt vælge 4 forskellige.', // todo translate
 			'vitamins.exists' => 'Du har valgt et vitamin som ikke findes, hvordan ved vi ikke, prøv igen.' // todo translate
 		]);
 
@@ -41,9 +41,13 @@ class PickMixController extends Controller
 
 			// todo return success
 
-			return \Redirect::action('AccountController@getHome')->with('success', 'Dine vitaminer blev opdateret!'); // todo translate
+			return \Redirect::action('AccountController@getHome')
+			                ->with('success', 'Dine vitaminer blev opdateret!'); // todo translate
 		}
 
-		dd($request->get('vitamins', []));
+		\Session::put('vitamins', $request->get('vitamins'));
+		\Session::put('product_name', 'subscription');
+
+		return \Redirect::action('CheckoutController@getCheckout');
 	}
 }
