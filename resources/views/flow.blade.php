8d2898de9cd4fd7490d0ac1f484e3347ad8b477c
lasserafn <lasserafn@gmail.com>
2016-08-31 18:29:22 +0200

    Moved send button functionality to a click event

--- resources/views/flow.blade.php
+++ resources/views/flow.blade.php
@@ -1220,35 +1220,37 @@
 
 
 	<script>{{-- todo translate --}}
-		swal({
-			title: "Send anbefaling",
-			text: "Indtast din e-mail adresse:",
-			type: "input",
-			showCancelButton: true,
-			confirmButtonColor: "#3AAC87",
-			confirmButtonText: "Send",
-			cancelButtonText: "Annuller",
-			closeOnConfirm: false,
-			inputPlaceholder: "navn@email.dk",
-			showLoaderOnConfirm: true
-		}, function (inputValue) {
-			if (inputValue === false || inputValue === "") {
-				swal.showInputError("Du skal indtaste din e-mail!");
-				return false;
-			}
+	$("#send-by-mail-button").click(function () {
+			swal({
+				title: "Send anbefaling",
+				text: "Indtast din e-mail adresse:",
+				type: "input",
+				showCancelButton: true,
+				confirmButtonColor: "#3AAC87",
+				confirmButtonText: "Send",
+				cancelButtonText: "Annuller",
+				closeOnConfirm: false,
+				inputPlaceholder: "navn@email.dk",
+				showLoaderOnConfirm: true
+			}, function (inputValue) {
+				if (inputValue === false || inputValue === "") {
+					swal.showInputError("Du skal indtaste din e-mail!");
+					return false;
+				}
 
-			setTimeout(function () {
-				swal({
-					title: "{{ trans('message.success-title') }}",
-					text: "Anbefalingen blev sendt!",
-					type: "success",
-					html: true,
-					allowOutsideClick: true,
-					confirmButtonText: "{{ trans('popup.button-close') }}",
-					confirmButtonColor: "#3AAC87",
-					timer: 6000
-				});
-			}, 2000);
+				setTimeout(function () {
+					swal({
+						title: "{{ trans('message.success-title') }}",
+						text: "Anbefalingen blev sendt!",
+						type: "success",
+						html: true,
+						allowOutsideClick: true,
+						confirmButtonText: "{{ trans('popup.button-close') }}",
+						confirmButtonColor: "#3AAC87",
+						timer: 6000
+					});
+				}, 2000);
+			});
 		});
 	</script>
 @endsection
