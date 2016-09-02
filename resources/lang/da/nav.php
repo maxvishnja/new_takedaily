7279d563fed10e64f611e488a33c2176409ecd41
lasserafn <lasserafn@gmail.com>
2016-09-02 15:55:27 +0200

    Gave more space for menu items

--- resources/lang/da/nav.php
+++ resources/lang/da/nav.php
@@ -11,6 +11,6 @@ return [
 	'links'   => '<li><a href="/gifting">Giv i gave</a></li>
 				  <li><a href="/sadan-virker-det">Sådan virker det</a></li>
 				  <li class="hidden-md"><a href="/test-and-quality">Test &amp; kvalitet</a></li>
-				  <li class="hidden-md"><a href="/about">Om os</a></li>
-				  <li><a href="/fra-a-til-zink">Fra A-Zink</a></li>'
+				  <li><a href="/about">Om os</a></li>
+				  <li><a href="/fra-a-til-zink">Fra A-Zink</a></li>' // todo move overflow items into a hamburger! -
 ];
