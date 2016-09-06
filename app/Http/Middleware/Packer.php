6efc613c0bae53ac5316b0e2bc9f681a71de4079
lasserafn <lasserafn@gmail.com>
2016-09-06 12:13:10 +0200

    Packer middleware setup

--- app/Http/Middleware/Packer.php
+++ app/Http/Middleware/Packer.php
@@ -6,6 +6,11 @@ use Closure;
 
 class Packer
 {
+
+	protected $except = [
+		'packer/login',
+	];
+
     /**
      * Handle an incoming request.
      *
@@ -15,6 +20,32 @@ class Packer
      */
     public function handle($request, Closure $next)
     {
+	    foreach ($this->except as $except) {
+		    if ($except !== '/') {
+			    $except = trim($except, '/');
+		    }
+
+		    if ($request->is($except)) {
+			    return $next($request);
+		    }
+	    }
+
+	    if ( \Auth::guard($guard)->guest() )
+	    {
+		    if ( $request->ajax() )
+		    {
+			    return response('Unauthorized.', 401);
+		    }
+		    else
+		    {
+			    return redirect()->guest('dashboard/login');
+		    }
+	    }
+
+	    if ( \Auth::user() && !\Auth::user()->isPacker() )
+	    {
+		    return response('Page not found.', 404);
+	    }
         return $next($request);
     }
 }
