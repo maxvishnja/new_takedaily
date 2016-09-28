4b2271d604717ac7aa6522a92a1dcdd96865d54c
lasserafn <lasserafn@gmail.com>
2016-09-28 21:33:41 +0200

    New pill names

--- app/Apricot/Libraries/PillLibrary.php
+++ app/Apricot/Libraries/PillLibrary.php
@@ -20,21 +20,21 @@ class PillLibrary
 		'3f' => 'White Code 20-1K',
 	];*/
 	public static $codes = [
-		'1a' => 'Red-White 17-63K/20-1K',
-		'1b' => 'Green-White 10-9K/20-1K',
-		'1c' => 'Blue-White 4-57K(B)20-1K',
-		'2a' => 'Buff 6 - 3K(D)',
-		'2b' => 'Light Blue 4 27K(A)',
-		'2c' => 'Green Semi Transp. 10-4K',
-		'2d' => 'Orange  Code 14-21K',
-		'2e' => 'Dark Yellow Code 19-155K',
-		'3a' => 'Green Opaque 10-567K',
-		'3b' => 'Swed.Orange 17-486K(C)',
-		'3c' => 'Yellow 19-103K(A)',
-		'3d' => 'Transparant Code 1-0K',
-		'3e' => 'Fish',
-		'3f' => 'White Code 20-1K',
-	    '3g' => ''
+		'1a' => 'Multi Basic',
+		'1b' => 'Multi Vitamin D+',
+		'1c' => 'Multi Vitamin D+ Extra',
+		'2a' => 'Mom & Baby',
+		'2b' => 'Shape Me up',
+		'2c' => 'Energy Booster',
+		'2d' => 'Immune Booster',
+		'2e' => 'Bone & joint',
+		'3a' => 'I\'m not a veggie fan',
+		'3b' => 'I don\'t Eat Carbs',
+		'3c' => 'The Dairy Portion',
+		'3d' => 'I am Vegetarian / Vegan',
+		'3e' => 'Premium Omega-3 Fish Oil',
+		'3f' => 'Not a Butter Lover',
+	    '3g' => 'Premium Omega-3 Chia Seed Oil'
 	];
 
 	public static function getPill($group, $item)
