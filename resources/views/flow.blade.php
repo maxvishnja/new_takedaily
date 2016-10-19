362c14e85286b7394f2d242dbfe577a4770c1a81
Lasse Rafn <lasserafn@gmail.com>
2016-10-19 11:48:08 +0200

    Fixed background pattern on landing, on mobile

--- resources/views/flow.blade.php
+++ resources/views/flow.blade.php
@@ -658,7 +658,7 @@
 							<div class="col-md-7">
 								<div class="tabs m-b-30">
 									<div class="options">
-										<div data-tab="#advises-label" class="tab tab-toggler tab--active">
+										<div data-tab="#advises-label-tab" class="tab tab-toggler tab--active">
 											Supplementer
 										</div>
 										<div data-tab="#advises-content" class="tab tab-toggler">Beskrivelse</div>
@@ -666,7 +666,10 @@
 										<div class="clear"></div>
 									</div>
 
-									<div id="advises-label" class="tab-block tab-block--active"></div>
+									<div id="advises-label-tab" class="tab-block tab-block--active">
+										<div id="advises-label"></div>
+										<div class="">Show if no oil and stuff..</div>{{-- todo select oil + double up --}}
+									</div>
 									<div id="advises-content" class="tab-block"></div>
 								</div>
 
