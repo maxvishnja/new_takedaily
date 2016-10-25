e3214ff74e776e84d44c59c980c86a1c49a56549
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:22:40 +0200

    Added label 2e

---
+++ resources/lang/da/label-2e.php
@@ -0,0 +1,31 @@
+<?php //
+return [
+	'name'        => 'Supplement til led',
+	'praises'     => [],
+	'ingredients' => 'Magnesiumhydroxid, dicalciumcitrate, K-Caps (hydroxypropylmethylcellulose, vand, farvestof (E171, E110)), dikaliumphosphate, L-ascorbinsyre, calciumcarbonat, antiklumpningsmiddel (magnesium stearate), manganogluconat, cholecalciferol',
+	'vitamins'    => [
+		[
+			'name'    => 'Vitamin D',
+			'amount'  => '10 µg',
+			'percent' => '200'
+		],
+		[
+			'name'    => 'Vitamin C',
+			'amount'  => '80 mg',
+			'percent' => '100'
+		]
+	],
+	'minerals'    => [
+		[
+			'name'    => 'Magnesium',
+			'amount'  => '175 mg',
+			'percent' => '47'
+		],
+		[
+			'name'    => 'Mangan',
+			'amount'  => '1 mg',
+			'percent' => '50'
+		],
+	],
+	'disclaimer'  => ''
+];
