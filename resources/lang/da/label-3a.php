a37ef67352625ab34b1b4d7bd12d748e787937a9
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:25:24 +0200

    Added label 3a

---
+++ resources/lang/da/label-3a.php
@@ -0,0 +1,40 @@
+<?php
+return [
+	'name'        => 'Supplement til lavt indtag af frugt og grønt',
+	'praises'     => [
+		'one'   => 'Bidrager til beskyttelse af celler mod oxidativt stress',
+		'two'   => 'Bidrager til den normale funktion af immunforsvaret',
+		'three' => 'Bidrager til at mindske træthed og udmattelse'
+	],
+	'ingredients' => 'Dikaliumphosphat, K-Caps (hydroxypropylmethylcellulose, vand, farvestof (E171, E141)), L-ascorbinsyre, fyldstof (microcrystalline cellulose), antiklumpningsmiddel (magnesium stearate), beta-caroten, pteroylmonoglutaminsyre',
+	'vitamins'    => [
+		[
+			'name'    => 'Vitamin A',
+			'amount'  => '200,4 µg',
+			'percent' => '25'
+		],
+		[
+			'name'    => 'Vitamin C',
+			'amount'  => '120 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Folsyre',
+			'amount'  => '300 µg',
+			'percent' => '150'
+		]
+	],
+	'minerals'    => [
+		[
+			'name'    => 'Kalium',
+			'amount'  => '317,1 mg',
+			'percent' => '16'
+		],
+		[
+			'name'    => 'Phosphor',
+			'amount'  => '126,3 mg',
+			'percent' => '18'
+		],
+	],
+	'disclaimer'  => ''
+];
