51b274e6b72374bfb7828589b0d91e6559779aeb
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:41:34 +0200

    Refactor..

--- resources/lang/da/label-3g.php
+++ resources/lang/da/label-3g.php
@@ -1,51 +1,51 @@
 <?php
 return [
-	'name'        => 'Chiafrøolie',
-	'praises'     => [
+	'name'           => 'Chiafrøolie',
+	'praises'        => [
 		'one'   => 'ALA bidrager til vedligehold af normale blodcholesterol niveauer. Den gavnlige effekt opnåes ved et dagligt indtag på 2 g ALA.',
 		'two'   => 'Erstatning af mættede fedtsyrer med umættede fedtsyrer i kosten medvirker til vedligeholdelse af normale kolesterolniveauer i blodet',
 		'three' => 'Højt indhold af omega-3 fedtsyrer',
-		'four' => 'Højt indhold af flerumættede fedtsyrer'
+		'four'  => 'Højt indhold af flerumættede fedtsyrer'
 	],
-	'ingredients' => 'Chiafrøolie, K-Caps (hydroxypropylmethylcellulose, vand), antiklumpemiddel (silica)',
+	'ingredients'    => 'Chiafrøolie, K-Caps (hydroxypropylmethylcellulose, vand), antiklumpemiddel (silica)',
 	'other-vitamins' => [
 		[
-			'name' => 'Chiaolie',
-			'amount' => '580 mg',
+			'name'    => 'Chiaolie',
+			'amount'  => '580 mg',
 			'percent' => '*'
 		],
 		[
-			'name' => 'Heraf',
-			'amount' => '',
+			'name'    => 'Heraf',
+			'amount'  => '',
 			'percent' => ''
 		],
 		[
-			'name' => 'Mættede fedtsyrer',
-			'amount' => '58,58 mg',
+			'name'    => 'Mættede fedtsyrer',
+			'amount'  => '58,58 mg',
 			'percent' => '*'
 		],
 		[
-			'name' => 'Enkeltumættede fedtsyrer',
-			'amount' => '45,24 mg',
+			'name'    => 'Enkeltumættede fedtsyrer',
+			'amount'  => '45,24 mg',
 			'percent' => '*'
 		],
 		[
-			'name' => 'Flerumættede fedtsyrer',
-			'amount' => '470,38 mg',
+			'name'    => 'Flerumættede fedtsyrer',
+			'amount'  => '470,38 mg',
 			'percent' => '*'
 		],
 		[
-			'name' => 'Heraf',
-			'amount' => '',
+			'name'    => 'Heraf',
+			'amount'  => '',
 			'percent' => ''
 		],
 		[
-			'name' => 'Alfalinolensyre (ALA)',
-			'amount' => '369,5 mg',
+			'name'    => 'Alfalinolensyre (ALA)',
+			'amount'  => '369,5 mg',
 			'percent' => '*'
 		],
 	],
-	'vitamins'    => [],
-	'minerals'    => [],
-	'disclaimer'  => '*Referenceindtag (RI) ikke fastlagt'
+	'vitamins'       => [],
+	'minerals'       => [],
+	'disclaimer'     => '*Referenceindtag (RI) ikke fastlagt'
 ];
