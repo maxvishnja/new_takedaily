ac08074ce922cdf26a6f4e87d861adfe2684e291
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:44:24 +0200

    Label product translations

--- resources/lang/da/label-product.php
+++ resources/lang/da/label-product.php
@@ -1,15 +1,41 @@
 <?php
 return [
-	'defaults' => [
-		'recommended-daily-use' => 'Anbefalet daglig dosis:',
-		'info'                  => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til vokse samt børn fra 11 år.',
-		'dose'                  => '1 kapsel taget med vand til et måltid',
-		'ingredients'           => 'Ingredienser',
-		'titles'                => [
-			'vitamins' => 'Vitaminer',
-			'amount'   => 'Mængde',
-			'percent'  => 'Procent',
-			'minerals' => 'Mineraler'
-		],
-	]
+	'recommended-daily-use' => 'Anbefalet daglig dosis:',
+	'info'                  => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til vokse samt børn fra 11 år.',
+	'dose'                  => '1 kapsel taget med vand til et måltid',
+	'ingredients'           => 'Ingredienser',
+	'titles'                => [
+		'vitamins' => 'Vitaminer',
+		'amount'   => 'Mængde',
+		'percent'  => 'Procent',
+		'minerals' => 'Mineraler'
+	],
+	'regular-description'           => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+										Anbefalet daglig dosis bør ikke overskrides.<br/>
+										Alle produkter er velegnede til veganere.<br/>
+										Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
+										Opbevares utilgængeligt for børn. <br/>
+										Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
+										Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
+										Opbevares tørt og køligt.',
+	'chiaoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+							Anbefalet daglig dosis bør ikke overskrides.<br/>
+							Alle produkter er velegnede til veganere.<br/>
+							Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
+							Opbevares utilgængeligt for børn. <br/>
+							Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
+							Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
+							Opbevares tørt og køligt.',
+	'fishoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+								Anbefalet daglig dosis bør ikke overskrides.<br/>
+								Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
+								Opbevares utilgængeligt for børn. <br/>
+								Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
+								Opbevares tørt og køligt.',
+	'expiration'            => 'Mindst holdbar til og med:',
+	'weight'                => 'Vægt:',
+	'batch'                 => 'Bach nr:',
+	'address'               => 'Take Daily, Vimmelskaftet 43, 1161 København K, Danmark',
+	'production'            => 'Produceret i Spanien'
+
 ];
