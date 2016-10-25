8a525843738393e3173cd5103bc9e2f3728871a0
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 14:44:42 +0200

    Removed double up from flow view

--- resources/views/flow-includes/views/steps/recommendation/show.blade.php
+++ resources/views/flow-includes/views/steps/recommendation/show.blade.php
@@ -11,21 +11,6 @@
 
 		<div id="advises-label-tab" class="tab-block tab-block--active">
 			<div id="advises-label"></div>
-
-			<div class="m-t-20 card" v-show="user_data.double_oil == 0">
-				<div class="card_content text-center">
-					<strong v-show="result.three == 'e' || result.four == 'e'">Tilføj 1000 mg premium fiskolie mere til din TakeDaily for kun 19 kr./mdr</strong>{{-- todo unhardcode --}}
-					<strong v-show="result.three == 'g' || result.four == 'g'">Tilføj 1000 mg premium chia seed olie mere til din TakeDaily for kun 19 kr./mdr</strong>{{-- todo unhardcode --}}
-
-					<a href="#" class="m-t-10 button button-green-border button-doubleup" v-on:click="addAdditionalOil($event);">
-						<span class="icon icon-double"></span>
-						<span>Få dobbelt op på olien</span>{{-- todo translate --}}
-					</a>
-
-					{{--<a href="#" v-on:click="moreInfo('fishoil', $event);">Hvad er fiskeolie?</a>--}}
-					{{--<a href="#" v-on:click="moreInfo('chiaoil', $event);">Hvad er chiaolie?</a>--}}
-				</div>
-			</div>
 		</div>
 
 		<div id="advises-content" class="tab-block"></div>
