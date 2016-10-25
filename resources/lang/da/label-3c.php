53eb2d1a1c509a8f90c1ca1c7e9c2ee8f04e7e85
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:29:39 +0200

    Added label 3c

---
+++ resources/lang/da/label-3c.php
@@ -0,0 +1,28 @@
+<?php
+return [
+	'name'        => ' Supplement til lavt indtag af mejeriprodukter',
+	'praises'     => [
+		'one'   => 'Bidrager til vedligeholdelse af normale knogler'
+	],
+	'ingredients' => 'Calciumcarbonat, K-Caps (hydroxypropylmethylcellulose, vand, farvestof (E172, E171)), antiklumpningsmiddel (magnesium stearate), cyanocobalamin, riboflavin',
+	'vitamins'    => [
+		[
+			'name'    => 'Riboflavin (B2)',
+			'amount'  => '2,1 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'B12',
+			'amount'  => '2,5 µg',
+			'percent' => '100'
+		],
+	],
+	'minerals'    => [
+		[
+			'name'    => 'Calcium',
+			'amount'  => '287 mg',
+			'percent' => '36'
+		]
+	],
+	'disclaimer'  => ''
+];
