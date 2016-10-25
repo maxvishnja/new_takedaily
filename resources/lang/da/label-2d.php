4e4a32e7524b2c996cdac4a0df4f75f238fdcf8c
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:21:26 +0200

    Added label 2d

---
+++ resources/lang/da/label-2d.php
@@ -0,0 +1,53 @@
+<?php
+return [
+	'name'        => 'Supplement til immunforsvaret',
+	'praises'     => [
+		'one' => 'Bidrager til immunsystemets normale funktion'
+	],
+	'ingredients' => 'Fyldstof (microcrystalline cellulose), L-ascorbinsyre, K-Caps (hydroxypropylmethylcellulose, vand, farvestof (E171, E110, E127)), zinkgluconat, L-selenmethionin, retinylacetat, antiklumpningsmiddel (magnesium stearate), cholecalciferol, cyanocobalamin, pyridoxal-5’-phosphat, pteroylmonoglutaminsyre',
+	'vitamins'    => [
+		[
+			'name'    => 'Vitamin A',
+			'amount'  => '800 µg',
+			'percent' => '100'
+		],
+		[
+			'name'    => 'Vitamin D',
+			'amount'  => '10 µg',
+			'percent' => '200'
+		],
+		[
+			'name'    => 'Vitamin C',
+			'amount'  => '120 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Vitamin B6',
+			'amount'  => '2,1 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Folsyre',
+			'amount'  => '300 µg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Vitamin B12',
+			'amount'  => '3,75 µg',
+			'percent' => '150'
+		]
+	],
+	'minerals'    => [
+		[
+			'name'    => 'Zink',
+			'amount'  => '6,6 mg',
+			'percent' => '66'
+		],
+		[
+			'name'    => 'Selen',
+			'amount'  => '83 µg',
+			'percent' => '150'
+		],
+	],
+	'disclaimer'  => ''
+];
