0af1073866f2f005c42ea80bfa8d658415530d2c
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 16:02:15 +0200

    Translated subscription mail

--- resources/views/emails/subscription.blade.php
+++ resources/views/emails/subscription.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.mail')
 
-@section('title', 'Vi har trukket penge') <!-- todo translate -->
-@section('summary', 'Du blev faktureret af TakeDaily') <!-- todo translate -->
+@section('title', trans('mails.subscription.title'))
+@section('summary', trans('mails.subscription.summary'))
 
 @section('content')
-
 	<p class="size-16" style='font-style: normal;font-weight: 400;Margin-bottom: 0;Margin-top: 16px;font-size: 16px;line-height: 24px;font-family: "Open Sans",sans-serif;color: #60666d;text-align: center;'>
-		Hej, da du har et abonnent hos os, har vi nu oprettet en ny ordre og trukket pengene for den. Du kan se dine ordre under "Mit TakeDaily" på vores hjemmeside.
+		{!! trans('mails.subscription.content') !!}
 	</p>
 @endsection
