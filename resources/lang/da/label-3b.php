77c0c739550bf31212c71c5a2ce67683944d4ffb
Lasse Rafn <lasserafn@gmail.com>
2016-10-25 18:27:55 +0200

    Added label 3b

---
+++ resources/lang/da/label-3b.php
@@ -0,0 +1,49 @@
+<?php
+return [
+	'name'        => 'Supplement til lavt indtag af brød, ris og pasta',
+	'praises'     => [
+		'one'   => 'Bidrager til normal kognitiv funktion',
+		'two'   => 'Bidrager til vedligeholdelse af normal hud'
+	],
+	'ingredients' => 'Fyldstof (microcrystalline cellulose), K-Caps (hydroxypropylmethylcellulose, vand, farvestof (E172)),  ferrobisglycinat, nicotinamid, antiklumpningsmiddel (magnesium stearate), pyridoxal-5’-phosphat, riboflavin, thiaminhydrochlorid, pteroylmonoglutaminsyre, kaliumjodid',
+	'vitamins'    => [
+		[
+			'name'    => 'Thiamin (B1)',
+			'amount'  => '1,65 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Riboflavin (B2)',
+			'amount'  => '2,1 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Niacin',
+			'amount'  => '24 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Vitamin B6',
+			'amount'  => '2,1 mg',
+			'percent' => '150'
+		],
+		[
+			'name'    => 'Folsyre',
+			'amount'  => '300 µg',
+			'percent' => '150'
+		]
+	],
+	'minerals'    => [
+		[
+			'name'    => 'Jern',
+			'amount'  => '10 mg',
+			'percent' => '71'
+		],
+		[
+			'name'    => 'Jod',
+			'amount'  => '75 µg',
+			'percent' => '50'
+		],
+	],
+	'disclaimer'  => ''
+];
