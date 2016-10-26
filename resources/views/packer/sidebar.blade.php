d533cc067a3df5560e5c2a6a835b27f3853758a5
Lasse Rafn <lasserafn@gmail.com>
2016-10-26 13:45:40 +0200

    Pending orders

--- resources/views/packer/sidebar.blade.php
+++ resources/views/packer/sidebar.blade.php
@@ -2,7 +2,7 @@
 	<div class="sidebar">
 		<ul class="widget widget-menu unstyled">
 			<li class="active"><a href="/packaging"><i class="menu-icon icon-dashboard"></i>Home</a></li>
-			<li><a href="/packaging/orders"><i class="menu-icon icon-money"></i>Orders @if($sidebar_numOrders > 0)<b class="label green pull-right">{{ $sidebar_numOrders }}</b>@endif</a></li>
+			<li><a href="/packaging/orders"><i class="menu-icon icon-money"></i>Pending orders @if($sidebar_numOrders > 0)<b class="label green pull-right">{{ $sidebar_numOrders }}</b>@endif</a></li>
 		</ul>
 		<!--/.widget-nav-->
 	</div>
