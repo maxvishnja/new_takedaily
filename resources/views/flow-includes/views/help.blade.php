da68109e19e6473338c71c8f91d5fd18a25d93bf
Lasse Rafn <lasserafn@gmail.com>
2016-10-28 15:21:10 +0200

    Demoing the flow helper

---
+++ resources/views/flow-includes/views/help.blade.php
@@ -0,0 +1,10 @@
+<div class="flow-helper">
+	<div class="m-b-10"><strong>Brug for hjælp?</strong></div>
+
+	<div>
+		<a href="tel:12345678" onclick="Tawk_API.toggle();" class="flow-helper_button">Chat</a>
+		<a href="tel:12345678" class="flow-helper_button">Ring til 1234 5678</a>
+	</div>
+
+	<div class="m-t-10">Hverdage 9-18</div>
+</div>
--- resources/views/flow.blade.php
+++ resources/views/flow.blade.php
@@ -12,22 +12,25 @@
 	<div id="app" class="flow-container">
 		@include('flow-includes.views.progress')
 
-		<div class="container">
+		<div style="position: relative">
+			@include('flow-includes.views.help')
 
-			<div class="flow-step-back" v-bind:class="{ 'clickable': step > 1 || sub_step > 1}">
-				<a href="javascript: void(0);" v-on:click="previousStep();">{{ trans('flow.back') }}</a>
+			<div class="container">
+				<div class="flow-step-back" v-bind:class="{ 'clickable': step > 1 || sub_step > 1}">
+					<a href="javascript: void(0);" v-on:click="previousStep();">{{ trans('flow.back') }}</a>
+				</div>
+
+				<form method="post" action="">
+					@include('flow-includes.views.steps.one')
+					@include('flow-includes.views.steps.two')
+					@include('flow-includes.views.steps.three')
+					@include('flow-includes.views.steps.four')
+
+					{{ csrf_field() }}
+					<input type="hidden" name="product_name"
+						   value="{{ Session::get('force_product_name', false) ? ( Session::get('product_name', 'subscription')) : 'subscription' }}"/>
+				</form>
 			</div>
-
-			<form method="post" action="">
-				@include('flow-includes.views.steps.one')
-				@include('flow-includes.views.steps.two')
-				@include('flow-includes.views.steps.three')
-				@include('flow-includes.views.steps.four')
-
-				{{ csrf_field() }}
-				<input type="hidden" name="product_name"
-					   value="{{ Session::get('force_product_name', false) ? ( Session::get('product_name', 'subscription')) : 'subscription' }}"/>
-			</form>
 		</div>
 	</div>
 
