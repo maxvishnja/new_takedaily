d38caec2a4bb8ad16e98a987ab48e4ce1d027190
Lasse Rafn <lasserafn@gmail.com>
2016-10-31 10:34:42 +0100

    Allow simple promo

--- resources/views/includes/promo.blade.php
+++ resources/views/includes/promo.blade.php
@@ -9,10 +9,26 @@
 	<div class="m-t-30">
 		<h4>{{ trans('promo.advantages') }}</h4>
 		<ul>
-			<li><span class="icon icon-check"></span> {{ trans('promo.custom-fit') }}</li>
-			<li><span class="icon icon-check"></span> {{ trans('promo.free-shipping') }}</li>
-			<li><span class="icon icon-check"></span> {{ trans('promo.no-cuffs') }}</li>
-			<li><span class="icon icon-check"></span> {{ trans('promo.direct-delivery') }}</li>
+			@if(trans('promo.custom-fit') != '')
+				<li><span class="icon icon-check"></span> {{ trans('promo.custom-fit') }}</li>
+			@endif
+
+			@if(trans('promo.custom-fit') != '')
+				<li><span class="icon icon-check"></span> {{ trans('promo.free-shipping') }}</li>
+			@endif
+
+			@if(trans('promo.custom-fit') != '')
+				<li><span class="icon icon-check"></span> {{ trans('promo.no-cuffs') }}</li>
+			@endif
+
+			@if(trans('promo.custom-fit') != '')
+				<li><span class="icon icon-check"></span> {{ trans('promo.direct-delivery') }}</li>
+			@endif
 		</ul>
+	</div>
+
+	<div class="m-t-20">
+		<p>Vores kunder elsker os!</p>
+		<a target="_blank" rel="nofollow" href="https://trustpilot.com/review/{{ Request::getHost() }}"><img src="{{ asset('/images/tp/4.png') }}" height="24" alt="Trustpilot"></a>
 	</div>
 </div>
