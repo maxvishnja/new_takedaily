4fe0fd4f953fe7c1c33758d101db38f0029b2540
Lasse Rafn <lasserafn@gmail.com>
2016-10-31 09:55:46 +0100

    NL nav translations

--- resources/lang/nl/nav.php
+++ resources/lang/nl/nav.php
@@ -14,5 +14,44 @@ return [
 				  <li><a href="/page/sadan-virker-det">Sådan virker det</a></li>
 				  <li class="hidden-md"><a href="/page/test-kvalitet">Test &amp; kvalitet</a></li>
 				  <li><a href="/page/about">Om os</a></li>
-				  <li><a href="/page/fra-a-til-zink">Fra A-Zink</a></li>' // todo move overflow items into a hamburger! -
+				  <li><a href="/page/fra-a-til-zink">Fra A-Zink</a><div class="submenu" style="min-width: 55%;">
+				  <div class="row">
+				  <div class="col-md-4">
+				  <ul>
+				  <li><a href="/page/vitamin-zink">Zink</a></li>
+				  	<li><a href="/page/vitamin-selen">Selen</a></li>
+				  	<li><a href="/page/vitamin-mangan">Mangan</a></li>
+				  	<li><a href="/page/vitamin-magnesium">Magnesium</a></li>
+				  	<li><a href="/page/vitamin-krom">Krom</a></li>
+				  	<li><a href="/page/vitamin-kobber">Kobber</a></li>
+				  	<li><a href="/page/vitamin-kalium">Kalium</a></li>
+				  	<li><a href="/page/vitamin-jod">Jod</a></li>
+				  	</ul>
+				  	</div>
+				  <div class="col-md-4">
+				  <ul>
+				  	<li><a href="/page/vitamin-jern">Jern</a></li>
+				  	<li><a href="/page/vitamin-fosfor">Fosfor</a></li>
+				  	<li><a href="/page/vitamin-calcium">Calcium</a></li>
+				  	<li><a href="/page/vitamin-k">Vitamin K</a></li>
+				  	<li><a href="/page/vitamin-e">Vitamin E</a></li>
+				  	<li><a href="/page/vitamin-d3">Vitamin D3</a></li>
+				  	<li><a href="/page/vitamin-c">Vitamin C</a></li>
+				  	<li><a href="/page/vitamin-a">Vitamin A</a></li>
+				  	</ul>
+					</div>
+				  <div class="col-md-4">
+				  
+				  <ul>
+				  	<li><a href="/page/vitamin-b12-kobalamin">Vitamin B12</a></li> <!--  (Kobalamin) -->
+				  	<li><a href="/page/vitamin-b9-folatfolsyre">Vitamin B9</a></li> <!--  (Folat/Folsyre) -->
+				  	<li><a href="/page/vitamin-b8-biotin">Vitamin B8</a></li> <!--  (Biotin) -->
+				  	<li><a href="/page/vitamin-b6-pyridoxin">Vitamin B6</a></li> <!--  (Pyridoxin) -->
+				  	<li><a href="/page/vitamin-b5-pantotensyre">Vitamin B5</a></li> <!--  (Pantotensyre) -->
+				  	<li><a href="/page/vitamin-b3-niacin">Vitamin B3</a></li> <!--  (Niacin) -->
+				  	<li><a href="/page/vitamin-b2-riboflavin">Vitamin B2</a></li> <!--  (Riboflavin) -->
+				  	<li><a href="/page/vitamin-b1-tiamin">Vitamin B1</a></li> <!--  (Tiamin) -->
+				  	</ul>
+					</div>
+				  </div></div></li>' // todo move overflow items into a hamburger! -
 ];
