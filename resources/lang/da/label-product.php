69c1d1e772b423f1cea696e697d38fbbc88c9015
Lasse Rafn <lasserafn@gmail.com>
2016-11-01 14:36:40 +0100

    Stuff

--- resources/lang/da/label-product.php
+++ resources/lang/da/label-product.php
@@ -1,20 +1,20 @@
 <?php
 
-return array (
-  'recommended-daily-use' => 'Anbefalet daglig dosis:',
-  'info' => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til vokse samt børn fra 11 år. Bør ikke stå i stedet for en sund og varieret kost',
-  'dose' => '1 kapsel taget med vand til et måltid',
-  'ingredients' => 'Ingredienser',
-  'titles' => 
-  array (
-    'vitamins' => 'Vitaminer',
-    'amount' => 'Mængde',
-    'percent' => 'Procent',
-    'minerals' => 'Mineraler',
-  ),
-  'for-me' => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
-  'produced-for' => 'Produceret og tilpasset til',
-  'regular-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+return [
+	'recommended-daily-use' => 'Anbefalet daglig dosis:',
+	'info'                  => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til vokse samt børn fra 11 år. Bør ikke stå i stedet for en sund og varieret kost',
+	'dose'                  => '1 kapsel taget med vand til et måltid',
+	'ingredients'           => 'Ingredienser',
+	'titles'                =>
+		[
+			'vitamins' => 'Vitaminer',
+			'amount'   => 'Mængde',
+			'percent'  => 'Procent',
+			'minerals' => 'Mineraler',
+		],
+	'for-me'                => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
+	'produced-for'          => 'Produceret og tilpasset til',
+	'regular-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 										Anbefalet daglig dosis bør ikke overskrides.<br/>
 										Alle produkter er velegnede til veganere.<br/>
 										Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
@@ -22,7 +22,7 @@ return array (
 										Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 										Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
 										Opbevares tørt og køligt.',
-  'chiaoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+	'chiaoil-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 							Anbefalet daglig dosis bør ikke overskrides.<br/>
 							Alle produkter er velegnede til veganere.<br/>
 							Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
@@ -30,16 +30,16 @@ return array (
 							Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 							Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
 							Opbevares tørt og køligt.',
-  'fishoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+	'fishoil-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 								Anbefalet daglig dosis bør ikke overskrides.<br/>
 								Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
 								Opbevares utilgængeligt for børn. <br/>
 								Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 								Opbevares tørt og køligt.',
-  'expiration' => 'Mindst holdbar til og med:',
-  'weight' => 'Vægt:',
-  'batch' => 'Bach nr:',
-  'address' => 'Take Daily, Vimmelskaftet 43, 1161 København K, Danmark',
-  'production' => 'Produceret i Spanien',
-  'good-for' => 'Bidrager til:',
-);
+	'expiration'            => 'Mindst holdbar til og med:',
+	'weight'                => 'Vægt:',
+	'batch'                 => 'Bach nr:',
+	'address'               => 'Take Daily, Vimmelskaftet 43, 1161 København K, Danmark',
+	'production'            => 'Produceret i Spanien',
+	'good-for'              => 'Bidrager til:',
+];
