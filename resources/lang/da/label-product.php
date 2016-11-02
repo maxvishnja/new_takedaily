bf9d4ea0f1ca3d55ac1d89b7f3c08dc2cadb84ac
Lasse Rafn <lasserafn@gmail.com>
2016-11-02 13:58:57 +0100

    Label trans

--- resources/lang/da/label-product.php
+++ resources/lang/da/label-product.php
@@ -1,27 +1,26 @@
 <?php
 
-return [
-	'recommended-daily-use' => '<strong>Anbefalet daglig dosis:</strong> Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til voksne fra 18 år. ',
-	'not-replace' => 'Kosttilskud bør ikke erstatte en sund og varieret kost.',
-	'dose'                  => '1 kapsel taget med vand til et måltid',
-	'ingredients'           => 'Ingredienser',
-	'titles'                =>
-		[
-			'vitamins' => 'Vitaminer',
-			'amount'   => 'Mængde',
-			'percent'  => 'Procent',
-			'minerals' => 'Mineraler',
-		],
-	'for-me'                => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
-	'produced-for'          => 'Produceret og tilpasset til',
-	'expiration'            => 'Mindst holdbar til og med:',
-	'weight'                => 'Vægt:',
-	'batch'                 => 'Bach nr:',
-	'address'               => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
-	'production'            => 'Produceret i Spanien',
-	'good-for'              => 'Bidrager til',
-	'contains'              => 'Kosttilskud',
-	'amount-daily'          => 'Tag 1 blisterpakning dagligt.',
-	'when-to-take'          => 'Tages i forbindelse med et måltid.',
-	'health-disclaimer'     => 'Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller personer under 18 år. ',
-];
+return array (
+  'recommended-daily-use' => '<strong>Anbefalet daglig dosis:</strong> Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til voksne fra 18 år. ',
+  'not-replace' => 'Kosttilskud bør ikke erstatte en sund og varieret kost.',
+  'dose' => '1 kapsel taget med vand til et måltid',
+  'ingredients' => 'Ingredienser',
+  'titles' => 
+  array (
+    'vitamins' => 'Vitaminer',
+    'amount' => 'Mængde',
+    'percent' => 'Procent',
+    'minerals' => 'Mineraler',
+  ),
+  'produced-for' => 'Produceret og tilpasset til',
+  'expiration' => 'Mindst holdbar til og med:',
+  'weight' => 'Vægt:',
+  'batch' => 'Bach nr:',
+  'address' => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
+  'production' => 'Produceret i Spanien',
+  'good-for' => 'Bidrager til',
+  'contains' => 'Kosttilskud',
+  'amount-daily' => 'Tag 1 blisterpakning dagligt.',
+  'when-to-take' => 'Tages i forbindelse med et måltid.',
+  'health-disclaimer' => 'Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller personer under 18 år. ',
+);
--- resources/lang/nl/label-product.php
+++ resources/lang/nl/label-product.php
@@ -2,7 +2,7 @@
 
 return array (
   'weight' => 'Gewicht',
-  'address' => 'Take Daily, Vimmelskaftet 43, 1161 København K, Danmark',
+  'address' => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
   'batch' => 'Batchnr.',
   'expiration' => 'Ten minste houdbaar tot einde:',
   'dose' => 'Voor volwassenen vanaf 18 jaar: Dagelijks 1 capsule, tenzij anders geadviseerd. Aanbevolen dosering niet overschrijden. Bij voorkeur bij een maaltijd met voldoende water innemen',
@@ -19,18 +19,7 @@ return array (
     'vitamins' => 'Vitamines',
   ),
   'recommended-daily-use' => 'De aanbevolen dagdosering :',
-  'regular-description' => 'Voedingssupplement <br/>
-Bevat 28 capsules. Open slechts 1 doordrukstrip per dag. <br/>
-Alle producten zijn geschikt voor vegetariërs en veganisten. <br/>
-Alle capsules gemaakt van HPMC, chemisch gemodificeerd cellulose. <br/>
-Een voedingssupplement is geen vervanging van een gevarieerde, gezonde en evenwichtige voeding. <br/>
-Bij gelijktijdig gebruik van andere supplementen, de gebruiksaanwijzing goed lezen, aangezien deze supplementen dezelde ingrediënten kunnen bevatten. In geval van overdosering raadpleeg uw arts.
-Raadpleeg in het geval van zwangerschap, lactatie, ziekte of medicijngebruik voor gebruik eerst een deskundige. <br/>
-Dit voedingssupplement is niet geschikt voor personen jonger dan 18 jaar. <br/>
-BEWAREN Droog en op
-kamertemperatuur bewaren. Na gebruik hoes weer om verpakking plaatsen. Buiten het bereik van jonge kinderen bewaren. Als de doordrukstrip beschadigd of open is, de supplementen niet gebruiken. <br/>',
   'produced-for' => 'Geproduceerd en afgestemd op de behoeften van:',
   'good-for' => 'Draagt bij aan',
-  'info' => 'Voedingsinformatie. 1 capsule bevat:',
   'contains' => 'Voedingssupplement',
 );
