3659c5499b6eeb53791c3424965249095088083f
Lasse Rafn <lasserafn@gmail.com>
2016-11-02 13:13:20 +0100

    Translations

--- resources/lang/da/label-product.php
+++ resources/lang/da/label-product.php
@@ -1,20 +1,20 @@
 <?php
 
-return [
-	'recommended-daily-use' => 'Anbefalet daglig dosis:',
-	'info'                  => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til vokse samt børn fra 11 år. 1 blisterpakning dagligt til voksne samt børn fra 11 år og derover. Tages i forbindelse med et måltid. Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. Kosttilskud bør ikke erstatte en varieret kost.',
-	'dose'                  => '1 kapsel taget med vand til et måltid',
-	'ingredients'           => 'Ingredienser',
-	'titles'                =>
-		[
-			'vitamins' => 'Vitaminer',
-			'amount'   => 'Mængde',
-			'percent'  => 'Procent',
-			'minerals' => 'Mineraler',
-		],
-	'for-me'                => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
-	'produced-for'          => 'Produceret og tilpasset til',
-	'regular-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+return array (
+  'recommended-daily-use' => 'Anbefalet daglig dosis:',
+  'info' => 'Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til voksne fra 18 år. <strong>Tag 1 blisterpakning dagligt</strong>. Tages i forbindelse med et måltid. Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller personer under 18 år. <strong>Kosttilskud bør ikke erstatte en sund og varieret kost</strong>.',
+  'dose' => '1 kapsel taget med vand til et måltid',
+  'ingredients' => 'Ingredienser',
+  'titles' => 
+  array (
+    'vitamins' => 'Vitaminer',
+    'amount' => 'Mængde',
+    'percent' => 'Procent',
+    'minerals' => 'Mineraler',
+  ),
+  'for-me' => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
+  'produced-for' => 'Produceret og tilpasset til',
+  'regular-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 										Anbefalet daglig dosis bør ikke overskrides.<br/>
 										Alle produkter er velegnede til veganere.<br/>
 										Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
@@ -22,7 +22,7 @@ return [
 										Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 										Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
 										Opbevares tørt og køligt.',
-	'chiaoil-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+  'chiaoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 							Anbefalet daglig dosis bør ikke overskrides.<br/>
 							Alle produkter er velegnede til veganere.<br/>
 							Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
@@ -30,17 +30,17 @@ return [
 							Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 							Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
 							Opbevares tørt og køligt.',
-	'fishoil-description'   => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
+  'fishoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
 								Anbefalet daglig dosis bør ikke overskrides.<br/>
 								Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
 								Opbevares utilgængeligt for børn. <br/>
 								Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
 								Opbevares tørt og køligt.',
-	'expiration'            => 'Mindst holdbar til og med:',
-	'weight'                => 'Vægt:',
-	'batch'                 => 'Bach nr:',
-	'address'               => 'Take Daily, Vimmelskaftet 43, 1161 København K, Danmark',
-	'production'            => 'Produceret i Spanien',
-	'good-for'              => 'Bidrager til:',
-	'contains'              => 'Kosttilskud'
-];
+  'expiration' => 'Mindst holdbar til og med:',
+  'weight' => 'Vægt:',
+  'batch' => 'Bach nr:',
+  'address' => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
+  'production' => 'Produceret i Spanien',
+  'good-for' => 'Bidrager til:',
+  'contains' => 'Kosttilskud',
+);
