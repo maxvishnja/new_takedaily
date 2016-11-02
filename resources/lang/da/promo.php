2ce6f525e2479a99efdd769276198c7f2eaf8a06
Lasse Rafn <lasserafn@gmail.com>
2016-11-02 15:56:31 +0100

    Added TP text

--- resources/lang/da/promo.php
+++ resources/lang/da/promo.php
@@ -1,13 +1,14 @@
 <?php
 
-return array (
-  'free-shipping' => 'Gratis fragt',
-  'no-cuffs' => 'Ingen binding',
-  'direct-delivery' => 'Leveres i postkassen',
-  'advantages' => 'Fordele',
-  'customer_service' => 'Kundeservice',
-  'need-help' => 'Har du brug for hjælp?',
-  'contact' => 'Ring på tlf.: 48 16 54 09',
-  'opening-hours' => '(mandag-fredag kl. 9-15)',
-  'custom-fit' => 'Skræddersyet til dig',
-);
+return [
+	'trustpilot'       => 'Vores kunder elsker os!',
+	'free-shipping'    => 'Gratis fragt',
+	'no-cuffs'         => 'Ingen binding',
+	'direct-delivery'  => 'Leveres i postkassen',
+	'advantages'       => 'Fordele',
+	'customer_service' => 'Kundeservice',
+	'need-help'        => 'Har du brug for hjælp?',
+	'contact'          => 'Ring på tlf.: 48 16 54 09',
+	'opening-hours'    => '(mandag-fredag kl. 9-15)',
+	'custom-fit'       => 'Skræddersyet til dig',
+];
