bd5e88e6ea31ea5a466b72669e649d8869b5ee3f
Lasse Rafn <lasserafn@gmail.com>
2016-11-02 13:48:31 +0100

    Stuff

--- resources/lang/da/label-product.php
+++ resources/lang/da/label-product.php
@@ -1,49 +1,27 @@
 <?php
 
-return array (
-  'recommended-daily-use' => '<strong>Anbefalet daglig dosis:</strong> Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til voksne fra 18 år. ',
-  'info' => '<strong>Kosttilskud bør ikke erstatte en sund og varieret kost</strong>.',
-  'dose' => '1 kapsel taget med vand til et måltid',
-  'ingredients' => 'Ingredienser',
-  'titles' => 
-  array (
-    'vitamins' => 'Vitaminer',
-    'amount' => 'Mængde',
-    'percent' => 'Procent',
-    'minerals' => 'Mineraler',
-  ),
-  'for-me' => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
-  'produced-for' => 'Produceret og tilpasset til',
-  'regular-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
-										Anbefalet daglig dosis bør ikke overskrides.<br/>
-										Alle produkter er velegnede til veganere.<br/>
-										Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
-										Opbevares utilgængeligt for børn. <br/>
-										Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
-										Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
-										Opbevares tørt og køligt.',
-  'chiaoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
-							Anbefalet daglig dosis bør ikke overskrides.<br/>
-							Alle produkter er velegnede til veganere.<br/>
-							Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
-							Opbevares utilgængeligt for børn. <br/>
-							Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
-							Alle kapsler er fremstillet af HPMC, kemisk modificeret cellulose. <br/>
-							Opbevares tørt og køligt.',
-  'fishoil-description' => 'Denne kasse indeholder dine kosttilskud for 28 dage, åben 1 lomme pr. dag.<br/>
-								Anbefalet daglig dosis bør ikke overskrides.<br/>
-								Kosttilskud må ikke stå i stedet for en sund og varieret kost.<br/>
-								Opbevares utilgængeligt for børn. <br/>
-								Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller børn under 1 år. <br/>
-								Opbevares tørt og køligt.',
-  'expiration' => 'Mindst holdbar til og med:',
-  'weight' => 'Vægt:',
-  'batch' => 'Bach nr:',
-  'address' => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
-  'production' => 'Produceret i Spanien',
-  'good-for' => 'Bidrager til',
-  'contains' => 'Kosttilskud',
-  'amount-daily' => 'Tag 1 blisterpakning dagligt.',
-  'when-to-take' => 'Tages i forbindelse med et måltid.',
-  'health-disclaimer' => 'Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller personer under 18 år. ',
-);
+return [
+	'recommended-daily-use' => '<strong>Anbefalet daglig dosis:</strong> Næringsindhold pr. anbefalet daglig dosis angivet i mængde og procent af referenceindtag (RI) til voksne fra 18 år. ',
+	'not-replace' => 'Kosttilskud bør ikke erstatte en sund og varieret kost.',
+	'dose'                  => '1 kapsel taget med vand til et måltid',
+	'ingredients'           => 'Ingredienser',
+	'titles'                =>
+		[
+			'vitamins' => 'Vitaminer',
+			'amount'   => 'Mængde',
+			'percent'  => 'Procent',
+			'minerals' => 'Mineraler',
+		],
+	'for-me'                => 'Hej :name, her er en lille tekst til dit label. Pt. er der ikke noget rigtigt infromation her, men det kommer der snart.',
+	'produced-for'          => 'Produceret og tilpasset til',
+	'expiration'            => 'Mindst holdbar til og med:',
+	'weight'                => 'Vægt:',
+	'batch'                 => 'Bach nr:',
+	'address'               => 'Take Daily ApS - Vimmelskaftet 43 - 1161 København K',
+	'production'            => 'Produceret i Spanien',
+	'good-for'              => 'Bidrager til',
+	'contains'              => 'Kosttilskud',
+	'amount-daily'          => 'Tag 1 blisterpakning dagligt.',
+	'when-to-take'          => 'Tages i forbindelse med et måltid.',
+	'health-disclaimer'     => 'Dagsdosis bør ikke overskrides. Bør kun efter aftale med læge eller sundhedsplejerske anvendes af gravide eller personer under 18 år. ',
+];
--- resources/views/pdf/sticker.blade.php
+++ resources/views/pdf/sticker.blade.php
@@ -106,7 +106,7 @@
 		@endif
 		<strong>{{ trans('label-product.recommended-daily-use') }}</strong>
 			{!! trans('label-product.info') !!}<br/>
-			{!! trans('label-product.recommended-daily-use') !!} <strong>{!! trans('label-product.amount-daily') !!}</strong> {!! trans('label-product.when-to-take') !!} {!! trans('label-product.health-disclaimer') !!} <strong>{!! trans('label-product.max-limit') !!}</strong>
+			{!! trans('label-product.recommended-daily-use') !!} <strong>{!! trans('label-product.amount-daily') !!}</strong> {!! trans('label-product.when-to-take') !!} {!! trans('label-product.health-disclaimer') !!} <strong>{!! trans('label-product.not-replace') !!}</strong>
 			{!! trans('label-product.batch') !!} · {!! trans('label-product.weight') !!} · {!! trans('label-product.expiration') !!}
 			<br/>
 		<div><strong>{{ trans('label-product.contains') }}</strong> · {{ trans('label-product.address') }}</div>
