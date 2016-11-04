04fd15fe1b3195dceaadaf8534f6b425d593995e
Lasse Rafn <lasserafn@gmail.com>
2016-11-04 18:12:52 +0100

    Changed stuff

--- resources/lang/nl/label-1a.php
+++ resources/lang/nl/label-1a.php
@@ -1,137 +1,136 @@
 <?php
 
-return array (
-  'minerals' => 
-  array (
-    0 => 
-    array (
-      'amount' => '166,6 mg',
-      'name' => 'Calcium',
-      'percent' => '20',
-    ),
-    1 => 
-    array (
-      'amount' => '128,7 mg',
-      'name' => 'Fosfor',
-      'percent' => '18',
-    ),
-    2 => 
-    array (
-      'amount' => '7 mg',
-      'name' => 'IJzer',
-      'percent' => '50',
-    ),
-    3 => 
-    array (
-      'amount' => '2,5 mg',
-      'name' => 'Zink',
-      'percent' => '25',
-    ),
-    4 => 
-    array (
-      'amount' => '27,5 µg',
-      'name' => 'Seleen',
-      'percent' => '50',
-    ),
-    5 => 
-    array (
-      'amount' => '20 µg',
-      'name' => 'Chroom',
-      'percent' => '50',
-    ),
-    6 => 
-    array (
-      'amount' => '75 µg',
-      'name' => 'Jodium',
-      'percent' => '50',
-    ),
-  ),
-  'vitamins' => 
-  array (
-    0 => 
-    array (
-      'amount' => '400 µg',
-      'name' => 'Vitamine A',
-      'percent' => '50',
-    ),
-    1 => 
-    array (
-      'amount' => '2,5 µg',
-      'name' => 'Vitamine D',
-      'percent' => '50',
-    ),
-    10 => 
-    array (
-      'amount' => '1,25 µg',
-      'name' => 'Vitamine B12',
-      'percent' => '50',
-    ),
-    11 => 
-    array (
-      'amount' => '25 µg',
-      'name' => 'Vitamine B8 (biotine)',
-      'percent' => '50',
-    ),
-    12 => 
-    array (
-      'amount' => '3 mg',
-      'name' => 'Vitamine B5',
-      'percent' => '50
-',
-    ),
-    2 => 
-    array (
-      'amount' => '6 mg',
-      'name' => 'Vitamine E',
-      'percent' => '50',
-    ),
-    3 => 
-    array (
-      'amount' => '37,5 µg',
-      'name' => 'Vitamine K',
-      'percent' => '50',
-    ),
-    4 => 
-    array (
-      'amount' => '40 mg',
-      'name' => 'Vitamine C',
-      'percent' => '50',
-    ),
-    5 => 
-    array (
-      'amount' => '0,55 mg',
-      'name' => 'Vitamine B1',
-      'percent' => '50',
-    ),
-    6 => 
-    array (
-      'amount' => '0,7 mg',
-      'name' => 'Vitamine B2',
-      'percent' => '50',
-    ),
-    7 => 
-    array (
-      'amount' => '8 mg',
-      'name' => 'Vitamine B3',
-      'percent' => '50',
-    ),
-    8 => 
-    array (
-      'amount' => '0,7 mg',
-      'name' => 'Vitamine B6',
-      'percent' => '50',
-    ),
-    9 => 
-    array (
-      'amount' => '100 µg',
-      'name' => 'Vitamine B11 (foliumzuur)',
-      'percent' => '50',
-    ),
-  ),
-  'name' => 'Multivitaminen- & mineralensupplement ',
-  'ingredients' => 'vulstof (dicalciumfosfaat dihydraat), K-Caps (hydroxypropylmethycellulose, water, kleurstoffen (E129, E171)), ijzer bisglycinaat, L-ascorbinezuur, zinkgluconaat, DL-alfa-tocoferylacetaat, nicotinamide, L-selenomethionine, antiklontermiddel (magnesiumstearaat), calcium-D-pantothenaat, retinylacetaat, cyanocobalamine, cholecalciferol, fyllochinon (fytomenadion), pyridoxaal-5-fosfaat, riboflavine, thiaminehydrochloride, chroompicolinaat, pteroylmonoglutaminezuur, kaliumjodide, D-biotine',
-  'praises' => 
-  array (
-    'shield' => 'De Multi Basic is een multivitaminen- & mineralensupplement voor volwassenen vanaf 18 jaar. Het bevat een uitgebalanceerde combinatie van essentiële vitaminen en mineralen ter ondersteuning van een optimale gezondheid',
-  ),
-);
+return [
+	'minerals'    =>
+		[
+			0 =>
+				[
+					'amount'  => '166,6 mg',
+					'name'    => 'Calcium',
+					'percent' => '20',
+				],
+			1 =>
+				[
+					'amount'  => '128,7 mg',
+					'name'    => 'Fosfor',
+					'percent' => '18',
+				],
+			2 =>
+				[
+					'amount'  => '7 mg',
+					'name'    => 'IJzer',
+					'percent' => '50',
+				],
+			3 =>
+				[
+					'amount'  => '2,5 mg',
+					'name'    => 'Zink',
+					'percent' => '25',
+				],
+			4 =>
+				[
+					'amount'  => '27,5 µg',
+					'name'    => 'Seleen',
+					'percent' => '50',
+				],
+			5 =>
+				[
+					'amount'  => '20 µg',
+					'name'    => 'Chroom',
+					'percent' => '50',
+				],
+			6 =>
+				[
+					'amount'  => '75 µg',
+					'name'    => 'Jodium',
+					'percent' => '50',
+				],
+		],
+	'vitamins'    =>
+		[
+			0  =>
+				[
+					'amount'  => '400 µg',
+					'name'    => 'Vitamine A',
+					'percent' => '50',
+				],
+			1  =>
+				[
+					'amount'  => '2,5 µg',
+					'name'    => 'Vitamine D',
+					'percent' => '50',
+				],
+			10 =>
+				[
+					'amount'  => '1,25 µg',
+					'name'    => 'Vitamine B12',
+					'percent' => '50',
+				],
+			11 =>
+				[
+					'amount'  => '25 µg',
+					'name'    => 'Vitamine B8 (biotine)',
+					'percent' => '50',
+				],
+			12 =>
+				[
+					'amount'  => '3 mg',
+					'name'    => 'Vitamine B5',
+					'percent' => '50',
+				],
+			2  =>
+				[
+					'amount'  => '6 mg',
+					'name'    => 'Vitamine E',
+					'percent' => '50',
+				],
+			3  =>
+				[
+					'amount'  => '37,5 µg',
+					'name'    => 'Vitamine K',
+					'percent' => '50',
+				],
+			4  =>
+				[
+					'amount'  => '40 mg',
+					'name'    => 'Vitamine C',
+					'percent' => '50',
+				],
+			5  =>
+				[
+					'amount'  => '0,55 mg',
+					'name'    => 'Vitamine B1',
+					'percent' => '50',
+				],
+			6  =>
+				[
+					'amount'  => '0,7 mg',
+					'name'    => 'Vitamine B2',
+					'percent' => '50',
+				],
+			7  =>
+				[
+					'amount'  => '8 mg',
+					'name'    => 'Vitamine B3',
+					'percent' => '50',
+				],
+			8  =>
+				[
+					'amount'  => '0,7 mg',
+					'name'    => 'Vitamine B6',
+					'percent' => '50',
+				],
+			9  =>
+				[
+					'amount'  => '100 µg',
+					'name'    => 'Vitamine B11 (foliumzuur)',
+					'percent' => '50',
+				],
+		],
+	'name'        => 'Multivitaminen- & mineralensupplement ',
+	'ingredients' => 'vulstof (dicalciumfosfaat dihydraat), K-Caps (hydroxypropylmethycellulose, water, kleurstoffen (E129, E171)), ijzer bisglycinaat, L-ascorbinezuur, zinkgluconaat, DL-alfa-tocoferylacetaat, nicotinamide, L-selenomethionine, antiklontermiddel (magnesiumstearaat), calcium-D-pantothenaat, retinylacetaat, cyanocobalamine, cholecalciferol, fyllochinon (fytomenadion), pyridoxaal-5-fosfaat, riboflavine, thiaminehydrochloride, chroompicolinaat, pteroylmonoglutaminezuur, kaliumjodide, D-biotine',
+	'praises'     =>
+		[
+			'shield' => 'De Multi Basic is een multivitaminen- & mineralensupplement voor volwassenen vanaf 18 jaar. Het bevat een uitgebalanceerde combinatie van essentiële vitaminen en mineralen ter ondersteuning van een optimale gezondheid',
+		],
+];
