5ca742d4b516d90b1192edde5f616a9f1a18c133
Lasse Rafn <lasserafn@gmail.com>
2016-11-05 17:57:29 +0100

    Fix packagepicker button

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -3,31 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Package;
-use App\Vitamin;
 use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
 	public function get()
 	{
-		$packages         = Package::select(['id', 'identifier'])->orderBy('id', 'ASC')->get();
+		$packages = Package::select( [ 'id', 'identifier' ] )->orderBy( 'id', 'ASC' )->get();
 
 		return view( 'package-picker', compact( 'packages' ) );
 	}
 
-	public function select($id)
+	public function select( $id )
 	{
-
+		echo $id;
 	}
 
 	public function post( Request $request )
 	{
 		$this->validate( $request, [
-			'vitamins'       => 'min:3|max:5|exists:vitamins,id'
+			'vitamins' => 'min:3|max:5|exists:vitamins,id'
 		], [
-			'vitamins.min'          => 'Du har ikke valgt nok vitaminer, du skal mindst vælge :min forskellige.', // todo translate
-			'vitamins.max'          => 'Du har valgt for mange vitaminer, du kan maksimalt vælge :max forskellige.', // todo translate
-			'vitamins.exists'       => 'Du har valgt et vitamin som ikke findes, hvordan ved vi ikke, prøv igen.', // todo translate
+			'vitamins.min'    => 'Du har ikke valgt nok vitaminer, du skal mindst vælge :min forskellige.', // todo translate
+			'vitamins.max'    => 'Du har valgt for mange vitaminer, du kan maksimalt vælge :max forskellige.', // todo translate
+			'vitamins.exists' => 'Du har valgt et vitamin som ikke findes, hvordan ved vi ikke, prøv igen.', // todo translate
 		] );
 
 		$vitamins = collect( $request->get( 'vitamins' ) );
@@ -43,7 +42,7 @@ class PackageController extends Controller
 		}
 
 		\Session::forget( 'user_data' );
-		\Session::forget('flow-completion-token');
+		\Session::forget( 'flow-completion-token' );
 		\Session::put( 'package', '' );
 		\Session::put( 'product_name', 'package' );
 
