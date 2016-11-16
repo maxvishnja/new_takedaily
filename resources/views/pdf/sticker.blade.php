dfa97aeb40ccd136518adf8a947c53d6fbce6cee
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 15:29:12 +0100

    Margin, baby

--- resources/views/pdf/sticker.blade.php
+++ resources/views/pdf/sticker.blade.php
@@ -115,7 +115,7 @@
 					@endif
 					@if(trans("label-{$vitaminModel->code}.disclaimer") != '')
 						@if($vitaminModel->code == '3e')
-							<img src="/images/foa_logo.png" style="float: left; height: 8mm; margin-right: 3pt;" alt="Friends of the Sea"/>
+							<img src="/images/foa_logo.png" style="float: left; height: 8mm; margin-right: 3pt;margin-bottom: 3pt" alt="Friends of the Sea"/>
 						@endif
 						<p style="line-height: 1.1; font-weight: 200; color: #1A8562; font-size: 6pt; margin: 4pt 0 0;">{!! trans("label-{$vitaminModel->code}.disclaimer") !!}</p>
 					@endif
