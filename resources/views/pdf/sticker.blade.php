254613485d60781f88b12db84e728f9276b59d2c
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 15:16:12 +0100

    Foa logo

--- resources/views/pdf/sticker.blade.php
+++ resources/views/pdf/sticker.blade.php
@@ -10,6 +10,9 @@
 			@foreach($customer->getVitaminModels() as $vitaminModel)
 				<div class="vitamin">
 					<img src="{{ asset('/images/icons/pills/pill-' . $vitaminModel->code . '@2x.png') }}" alt="Vitamin icon" style="float: right;width:20pt; height: 19pt;">
+					@if($vitaminModel->code == '3e')
+						<img src="/images/foa_logo.png" style="float: right; height: 8mm; margin-left: 3pt;" alt="Friends of the Sea"/>
+					@endif
 					<h2 style="font-weight: bold; font-size: 10.5pt; margin: 0 0 2pt; color: #1A8562">{{ \App\Apricot\Libraries\PillLibrary::$codes[$vitaminModel->code] }}</h2>
 					<div style="margin: 4pt 0; font-size: 7pt; line-height: 1.2; color: #1A8562;">
 						<div style="margin-bottom: 2pt;">{!! trans('label-product.contains') !!}</div>
