6104f97cdbc78bc89052d8c3679113866c5353cd
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 15:26:02 +0100

    Fix locales

--- resources/lang/da/pick-package.php
+++ resources/lang/da/pick-package.php
@@ -1,85 +1,5 @@
 <?php
 
-return array (
-  'title' => 'Vælg din TakeDaily pakke',
-  'select-btn' => 'Vælg denne',
-  'custom-pill' => 'Vælg en pille',
-  'pills' => 
-  array (
-    '3a' => 'Some description of this pill',
-    '3b' => 'Some description of this pill',
-    '3c' => 'Some description of this pill',
-    '3d' => 'Some description of this pill',
-    '3e' => 'Some description of this pill',
-    '3f' => 'Some description of this pill',
-    '3g' => 'Some description of this pill',
-  ),
-  'select' => 
-  array (
-    'title' => 'Udfyld dine oplysninger..',
-  ),
-  'packages' => 
-  array (
-    'busy_life' => 
-    array (
-      'name' => 'Busy life/Energy',
-      'description' => 'B-vitaminer (B1, B2, B3, B5, B6 og B12) og jern er vigtige for en optimal energiomsætning i kroppen.
-
-B-vitaminer (B2, B3, B5, B6 og B12) og jern hjælp til at reducere træthed og udmattelse.
-
-Vitamin B5 bidrager til kroppens normale modstandsdygtighed overfor stress.',
-    ),
-    'relax' => 
-    array (
-      'name' => 'Relax',
-      'description' => 'Activeert de natuurlijke energie in het lichaam. Helpt bij vermoeidheid/moeheid. Goed voor de gemoedstoestand.',
-    ),
-    'sportsman_woman' => 
-    array (
-      'name' => 'Sportsman/-woman',
-      'description' => 'Activeert je natuurlijke energie in het lichaam. speelt een rol bij het behouden van sterke spieren. bevordert de weerstand tijdens en na fysieke inspanning.',
-    ),
-    'balanced_body' => 
-    array (
-      'name' => 'Body in Balance/Kroppen i Balance',
-      'description' => '',
-    ),
-    'immunity_boost' => 
-    array (
-      'name' => 'Immune Booster',
-      'description' => 'Vitamin A, -B6, -B12, -C, -D og folinsyre samt mineralerne zink og selen hjælper til opretholdelsen af et normalt immunforsvar.',
-    ),
-    'vega_power' => 
-    array (
-      'name' => 'Vega Power',
-      'description' => '',
-    ),
-    'women_menopause' => 
-    array (
-      'name' => 'Women in Menopause',
-      'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-    ),
-    'heart_bloodvessels' => 
-    array (
-      'name' => 'Heart & Vessels',
-      'description' => 'DHA og EPA bidrager til opretholdelsen af et normalt blodtryk og den normale funktion af hjertet.',
-    ),
-    'beauty_boost' => 
-    array (
-      'name' => 'Beauty Boost',
-      'description' => 'Selen og zink holder dit hår stærkt og bidrager til et skinnende hår.
-
-Selen og zink er godt for neglene.
-
-Vitamin A, -C og zink hjælper til den indre pleje af huden.',
-    ),
-    'pregnant' => 
-    array (
-      'name' => 'Pregnant woman & Pregnant to be',
-      'description' => 'Foliumzuur draagt bij tot de groei van het ongeboren kind tijdens de zwangerschap. De inname van docosahexaeenzuur (DHA) door de moeder draagt bij tot normale ontwikkeling van de hersenen en ogen bij het ongeboren kind.',
-    ),
-  ),
-);
 return [
 	'title'       => 'Vælg din TakeDaily pakke',
 	'select-btn'  => 'Vælg denne',
@@ -152,4 +72,3 @@ return [
 				],
 		],
 ];
->>>>>>> 9b61cc512c4d7a4706134a5f9becb3c1c8cda343
