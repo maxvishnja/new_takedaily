9b61cc512c4d7a4706134a5f9becb3c1c8cda343
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 15:22:46 +0100

    Fix locales

--- resources/lang/nl/pick-package.php
+++ resources/lang/nl/pick-package.php
@@ -1,66 +1,62 @@
 <?php
 
-return array (
-  'select' => 
-  array (
-    'title' => 'Vul jouw gegevens in..',
-  ),
-  'title' => 'Kies jouw TakeDaily pakket',
-  'select-btn' => 'Selecteer',
-  'packages' => 
-  array (
-    'heart_bloodvessels' => 
-    array (
-      'name' => 'Heart & Bloodvessels',
-      'description' => 'DHA en EPA dragen bij tot de instandhouding van de normale bloeddruk. draagt bij tot de normale werking van het hart.',
-    ),
-    'immunity_boost' => 
-    array (
-      'name' => 'Immune Booster',
-      'description' => 'zorgt mede voor een goede weerstand. helpt het immuunsysteem.',
-    ),
-    'pregnant' => 
-    array (
-      'description' => '-Foliumzuur draagt bij tot de groei van het ongeboren kind tijdens de zwangerschap. 
--De inname van docosahexaeenzuur (DHA) door de moeder draagt bij tot normale ontwikkeling van de hersenen en ogen bij het ongeboren kind.',
-      'name' => 'Pregnant woman & Pregnant to be',
-    ),
-    'beauty_boost' => 
-    array (
-      'name' => 'Beauty Boost',
-      'description' => 'Houdt je haar sterk voor het behoud van glanzend haar. goed voor nagels draagt bij aan het behoud van normale nagels. helpt bij de verzorging van de huid van binnenuit.',
-    ),
-    'balanced_body' => 
-    array (
-      'name' => 'Body in Balance',
-    ),
-    'sportsman_woman' => 
-    array (
-      'name' => 'Sportsman/-woman',
-      'description' => '-Activeert je natuurlijke energie in het lichaam. 
--Speelt een rol bij het behouden van sterke spieren. 
--Bevorderd de weerstand tijdens en na fysieke inspanning.',
-    ),
-    'vega_power' => 
-    array (
-      'name' => 'Vega Power
-',
-    ),
-    'women_menopause' => 
-    array (
-      'name' => 'Women in Menopause',
-      'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-    ),
-    'relax' => 
-    array (
-      'name' => 'Relax',
-      'description' => 'Activeert de natuurlijke energie in het lichaam. Helpt bij vermoeidheid/moeheid. Goed voor de gemoedstoestand.',
-    ),
-    'busy_life' => 
-    array (
-      'description' => 'activeert je natuurlijke energie in het lichaam. kan het gebrek aan energie helpen verminderen.',
-      'name' => 'Busy Life',
-    ),
-  ),
-  'custom-pill' => 'Kies een supplement',
-);
+return [
+	'select'      =>
+		[
+			'title' => 'Vul jouw gegevens in..',
+		],
+	'title'       => 'Kies jouw TakeDaily pakket',
+	'select-btn'  => 'Selecteer',
+	'packages'    =>
+		[
+			'heart_bloodvessels' =>
+				[
+					'name'        => 'Heart & Bloodvessels',
+					'description' => 'DHA en EPA dragen bij tot de instandhouding van de normale bloeddruk. draagt bij tot de normale werking van het hart.',
+				],
+			'immunity_boost'     =>
+				[
+					'name'        => 'Immune Booster',
+					'description' => 'zorgt mede voor een goede weerstand. helpt het immuunsysteem.',
+				],
+			'pregnant'           =>
+				[
+					'description' => '-Foliumzuur draagt bij tot de groei van het ongeboren kind tijdens de zwangerschap. <br/>-De inname van docosahexaeenzuur (DHA) door de moeder draagt bij tot normale ontwikkeling van de hersenen en ogen bij het ongeboren kind.',
+					'name'        => 'Pregnant woman & Pregnant to be',
+				],
+			'beauty_boost'       =>
+				[
+					'name'        => 'Beauty Boost',
+					'description' => 'Houdt je haar sterk voor het behoud van glanzend haar. goed voor nagels draagt bij aan het behoud van normale nagels. helpt bij de verzorging van de huid van binnenuit.',
+				],
+			'balanced_body'      =>
+				[
+					'name' => 'Body in Balance',
+				],
+			'sportsman_woman'    =>
+				[
+					'name'        => 'Sportsman/-woman',
+					'description' => '-Activeert je natuurlijke energie in het lichaam.<br/>-Speelt een rol bij het behouden van sterke spieren. <br/>-Bevorderd de weerstand tijdens en na fysieke inspanning.',
+				],
+			'vega_power'         =>
+				[
+					'name' => 'Vega Power',
+				],
+			'women_menopause'    =>
+				[
+					'name'        => 'Women in Menopause',
+					'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
+				],
+			'relax'              =>
+				[
+					'name'        => 'Relax',
+					'description' => 'Activeert de natuurlijke energie in het lichaam. Helpt bij vermoeidheid/moeheid. Goed voor de gemoedstoestand.',
+				],
+			'busy_life'          =>
+				[
+					'description' => 'activeert je natuurlijke energie in het lichaam. kan het gebrek aan energie helpen verminderen.',
+					'name'        => 'Busy Life',
+				],
+		],
+	'custom-pill' => 'Kies een supplement',
+];
