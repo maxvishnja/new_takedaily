ff0cdb565da5dbeab7512a009e9f9b7b275594cc
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 14:41:22 +0100

    Smaller ingredients

--- resources/views/pdf/sticker.blade.php
+++ resources/views/pdf/sticker.blade.php
@@ -108,7 +108,7 @@
 						</table>
 					@endif
 
-					<p style="line-height: 1.1; font-weight: 200; color: #1A8562; font-size: 6.5pt; margin: 2pt 0 0;">{{ trans('label-product.ingredients') }}: {{ trans("label-{$vitaminModel->code}.ingredients") }}</p>
+					<p style="line-height: 1.1; font-weight: 200; color: #1A8562; font-size: 6pt; margin: 2pt 0 0;">{{ trans('label-product.ingredients') }}: {{ trans("label-{$vitaminModel->code}.ingredients") }}</p>
 					@if(trans("label-{$vitaminModel->code}.disclaimer") != '')
 						<p style="line-height: 1.1; font-weight: 200; color: #1A8562; font-size: 6.5pt; margin: 4pt 0 0;">{!! trans("label-{$vitaminModel->code}.disclaimer") !!}</p>
 					@endif
