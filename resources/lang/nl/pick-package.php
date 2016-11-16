6b2210146fabe83cf167b66b2b26f354a76e80dd
Lasse Rafn <lasserafn@gmail.com>
2016-11-16 15:25:31 +0100

    Fix locales

--- resources/lang/nl/pick-package.php
+++ resources/lang/nl/pick-package.php
@@ -1,79 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-return array (
-  'select' => 
-  array (
-    'title' => 'Vul jouw gegevens in..',
-  ),
-  'title' => 'Kies jouw TakeDaily pakket',
-  'select-btn' => 'Selecteer',
-  'packages' => 
-  array (
-    'heart_bloodvessels' => 
-    array (
-      'name' => 'Hart & Bloedvaten',
-      'description' => 'DHA en EPA dragen bij tot de instandhouding van de normale bloeddruk. draagt bij tot de normale werking van het hart.',
-    ),
-    'immunity_boost' => 
-    array (
-      'name' => 'Immuunsysteem/ Immune Booster',
-      'description' => '<span class="icon icon-shield-flow flow-promise-icon"></span> <div class="flow-promise-text">Vitamine A, B6, foliumzuur, B12, C, D en de mineralen seleen en zink helpen het immuunsysteem</div>
-					<div class="clear"></div><span class="icon icon-shield-cell-flow flow-promise-icon"></span> <div class="flow-promise-text">Vitamine C en de mineralen seleen en zink helpen lichaamscellen te beschermen tegen invloeden van buitenaf</div>',
-    ),
-    'pregnant' => 
-    array (
-      'description' => '<div class="clear"></div><span class="icon icon-baby-flow flow-promise-icon"></span> <div class="flow-promise-text">Foliumzuur draagt bij tot de groei van het ongeboren kind tijdens de zwangerschap</div>
-  	   
-<span class="icon icon-brain-cog-flow flow-promise-icon"></span> <div class="flow-promise-text">De inname van docosahexaeenzuur (DHA) door de moeder draagt bij tot normale ontwikkeling van de hersenen en ogen bij het ongeboren kind.</div><!--// todo NB! Het gunstige effect wordt verkregen bij een dagelijkse inname van 200 mg DHA in aanvulling op de aanbevolen dagelijkse hoeveelheid voor omega-3 vetzuren voor volwassenen (250 mg EPA en DHA) -->
-
--Goed voor het energieniveau en weerstand',
-      'name' => 'Moeder & Baby/Pregnant & Pregnant to be',
-    ),
-    'beauty_boost' => 
-    array (
-      'name' => 'Huid, Haar & Nagel/Beauty Boost',
-      'description' => '-Seleen en zink houden je haar sterk en dragen bij aan het behoud van glanzend haar.
--Seleen en zink zijn goed voor nagels 
--Vitamine A, C en zink helpen bij de verzorging van de huid van binnenuit',
-    ),
-    'balanced_body' => 
-    array (
-      'name' => 'Body in Balance/Lichaam in Balans',
-    ),
-    'sportsman_woman' => 
-    array (
-      'name' => 'Sportman/-vrouw',
-      'description' => '-Activeert je natuurlijke energie in het lichaam. 
--Speelt een rol bij het behouden van sterke spieren. 
--Bevorderd de weerstand tijdens en na fysieke inspanning.',
-    ),
-    'vega_power' => 
-    array (
-      'name' => 'Vega Power
-',
-    ),
-    'women_menopause' => 
-    array (
-      'name' => 'Vrouwen in Menopauze',
-      'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-    ),
-    'relax' => 
-    array (
-      'name' => 'Relax',
-      'description' => 'Activeert de natuurlijke energie in het lichaam. Helpt bij vermoeidheid/moeheid. Goed voor de gemoedstoestand.',
-    ),
-    'busy_life' => 
-    array (
-      'description' => '<span class="icon icon-energy-flow flow-promise-icon"></span> <div class="flow-promise-text">De B-vitamines (B1, B2, B3, B5, B6 en B12) en ijzer activeren je natuurlijke energie in het lichaam</div>
-					<div class="clear"></div><span class="icon icon-lightning-flow flow-promise-icon"></span> <div class="flow-promise-text">De B-vitamines (B2, B3, B5, B6 en B12) en ijzer helpen om vermoeidheid en moeheid te verminderen</div>
-					<div class="clear"></div><span class="icon icon-stress-flow flow-promise-icon"></span> <div class="flow-promise-text">Vitamine B5 draagt bij aan een normale weerstand tegen stress</div>',
-      'name' => 'Druk leven/Energie',
-    ),
-  ),
-  'custom-pill' => 'Kies een supplement',
-);
-=======
 return [
 	'select'      =>
 		[
@@ -134,4 +60,3 @@ return [
 		],
 	'custom-pill' => 'Kies een supplement',
 ];
->>>>>>> 9b61cc512c4d7a4706134a5f9becb3c1c8cda343
