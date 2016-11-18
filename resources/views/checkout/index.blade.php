4919a540f63ab502e94f078602bcca57031a9c30
Lasse Rafn <lasserafn@gmail.com>
2016-11-18 22:57:18 +0100

    No more "guessing" when next order will be based on giftcard

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -223,7 +223,7 @@
 					@if($product->is_subscription == 1)
 						<div class="clear"></div>
 						<div class="visible-sm visible-xs">
-							{!! trans('checkout.index.disclaimer', ['date' => \Jenssegers\Date\Date::now()->addMonths($giftcard ? (round($giftcard->worth / $product->price) + 1) : 1)->format('j. M Y')]) !!}
+							{!! trans('checkout.index.disclaimer', ['date' => \Jenssegers\Date\Date::now()->addDays(28)->format('j. M Y')]) !!}
 						</div>
 					@endif
 
