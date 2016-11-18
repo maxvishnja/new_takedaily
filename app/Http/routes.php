255a026e4a543bbff2a0c3bf3d26a654e5b82d4f
Lasse Rafn <lasserafn@gmail.com>
2016-11-18 11:05:13 +0100

    Added titles to contents

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -312,7 +312,7 @@ Route::group( [ 'middleware' => 'web' ], function ()
 						break;
 				}
 
-				$ingredients .= '<p>' . trans( strtolower("label-{$index}{$combination}.ingredients") ) . '</p>';
+				$ingredients .= '<div><strong>' . trans( strtolower("label-{$index}{$combination}.name") ) . '</strong></div><p>' . trans( strtolower("label-{$index}{$combination}.ingredients") ) . '</p>';
 			}
 
 
