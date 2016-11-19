eed6226eacb44d27c814248d7f4777aea4ad284d
Lasse Rafn <lasserafn@gmail.com>
2016-11-20 00:02:32 +0100

    Now updating pregnancy weeks #2

--- app/Console/Commands/UpdatePregnancyWeeks.php
+++ app/Console/Commands/UpdatePregnancyWeeks.php
@@ -8,42 +8,48 @@ use Jenssegers\Date\Date;
 
 class UpdatePregnancyWeeks extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'customers:pregnancy-update';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'customers:pregnancy-update';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Update pregnancy weeks';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Update pregnancy weeks';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $customersWithBirthdates = Customer::select('customers.*', 'customer_attributes.value as pregnancy_date')->leftJoin('customer_attributes', 'customer_attributes.customer_id', '=', 'customers.id')->where('customer_attributes.identifier', 'user_data.pregnancy.date')->get();
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$customersWithBirthdates = Customer::select( 'customers.*', 'customer_attributes.value as pregnancy_date' )
+		                                   ->leftJoin( 'customer_attributes', 'customer_attributes.customer_id', '=', 'customers.id' )
+		                                   ->where( 'customer_attributes.identifier', 'user_data.pregnancy.date' )
+		                                   ->get();
 
-        foreach($customersWithBirthdates as $customer)
-        {
-			$customer->setCustomerAttribute('user_data.pregnancy.week', (new Date($customer->pregnancy_date))->diffInWeeks(), false);
-        }
-    }
+		foreach ( $customersWithBirthdates as $customer )
+		{
+			if ( $customer->pregnancy_date != '' )
+			{
+				$customer->setCustomerAttribute( 'user_data.pregnancy.week', ( new Date( $customer->pregnancy_date ) )->diffInWeeks(), false );
+			}
+		}
+	}
 }
