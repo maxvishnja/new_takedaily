4408490db2e655a7b388c2c75ce4572bcabdf8e5
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 17:40:33 +0100

    Fix cta and page designs

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -4,25 +4,27 @@
 
 @section('title', "{$page->title} - TakeDaily")
 
-@section('mainClasses', 'm-b-50 m-t-50')
-
-@section('header_top_additional')
-	@if( $page->layout == 'header' )
-		<h1 class="cms-title">{{ $page->title }}</h1>
-		@if( $page->sub_title != '')
-			<h2 class="cms-title">{{ $page->sub_title }}</h2>
-		@endif
-
-		@if( !Auth::user() || !Auth::user()->isUser() || ! Auth::user()->getCustomer() )
-			<div class="cta hidden-xs">
-				{!! trans('cms.sticky-cta') !!}
-			</div>
-		@endif
-	@endif
-@endsection
+@section('mainClasses', 'm-b-50')
 
 @section('content')
-	<div class="container">
+	@if( $page->layout == 'header' )
+		<div class="header_image">
+			<div class="container text-center">
+				<h1>{{ $page->title }}</h1>
+				@if( $page->sub_title != '')
+					<h2>{{ $page->sub_title }}</h2>
+				@endif
+
+				@if( !Auth::user() || !Auth::user()->isUser() || ! Auth::user()->getCustomer() )
+					<div class="cta hidden-xs">
+						{!! trans('cms.sticky-cta') !!}
+					</div>
+				@endif
+			</div>
+		</div>
+	@endif
+
+	<div class="container m-t-50">
 		<article>
 			@if( $page->layout == 'plain' )
 				<h1 class="cms-title">{{ $page->title }}</h1>
@@ -33,7 +35,8 @@
 			@elseif($page->layout == 'header')
 				<h1 class="cms-title" style="height: 0; margin: 0; color: transparent; position: absolute; text-indent: -99999999px; left: -9999999px;">{{ $page->title }}</h1>
 				@if( $page->sub_title != '')
-					<h2 class="cms-title" style="height: 0; margin: 0; color: transparent; position: absolute; text-indent: -99999999px; left: -9999999px;">{{ $page->sub_title }}</h2>
+					<h2 class="cms-title"
+						style="height: 0; margin: 0; color: transparent; position: absolute; text-indent: -99999999px; left: -9999999px;">{{ $page->sub_title }}</h2>
 				@endif
 			@endif
 			{!! $page->body !!}
@@ -50,13 +53,9 @@
 
 	@if( $page->layout == 'header' )
 		<style>
-			header.header--with-bg {
-				background-color:    #fff;
-				background-repeat:   no-repeat;
-				background-size:     cover;
-				background-position: center center;
-				background-image:    -webkit-linear-gradient(top, rgba(97, 97, 97, 0.64) 0%, rgba(51, 51, 51, 0.00) 100%), url({{ asset($page->top_image) }});
-				background-image:    linear-gradient(-180deg, rgba(97, 97, 97, 0.64) 0%, rgba(51, 51, 51, 0.00) 100%), url({{ asset($page->top_image) }});
+			.header_image {
+				background-image: -webkit-linear-gradient(top, rgba(97, 97, 97, 0.64) 0%, rgba(51, 51, 51, 0.00) 100%), url({{ asset($page->top_image) }});
+				background-image: linear-gradient(-180deg, rgba(97, 97, 97, 0.64) 0%, rgba(51, 51, 51, 0.00) 100%), url({{ asset($page->top_image) }});
 			}
 		</style>
 	@endif
@@ -66,16 +65,13 @@
 	@if( !Auth::user() || !Auth::user()->isUser() || ! Auth::user()->getCustomer() )
 		<script>
 			var ctaBlock = $(".cta");
-			var headerBlock = $("header.header--with-bg");
+			var headerBlock = $(".header_image");
 
-			$(window).scroll(function ()
-			{
-				if ($(this).scrollTop() > (headerBlock.height() - ctaBlock.outerHeight()))
-				{
+			$(window).scroll(function () {
+				if ($(this).scrollTop() > headerBlock.outerHeight()) {
 					ctaBlock.addClass('cta--sticky');
 				}
-				else
-				{
+				else {
 					ctaBlock.removeClass('cta--sticky');
 				}
 			});
