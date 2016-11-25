76ecb2c148405fb98b1d4889a2b983226a5accb3
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 21:33:16 +0100

    Minimal "My takedaily" page

--- resources/views/account/my-takedaily.blade.php
+++ resources/views/account/my-takedaily.blade.php
@@ -5,7 +5,10 @@
 @section('title', trans('account.personal.title'))
 
 @section('content')
+	<h1>{{ trans('account.home.header') }}</h1>
+
 	@foreach(Auth::user()->getCustomer()->getVitaminModels() as $vitamin)
 		<span class="icon pill-{{ $vitamin->code }}"></span>
 	@endforeach
+	<div class="m-t-10"><a href="/flow" class="button button--green">{{ trans('account.home.button-change') }}</a></div>
 @endsection
