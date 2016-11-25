0e716585f9a7367fccc5893b553fddd9a3e0947c
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 23:56:20 +0100

    Nav trans

--- resources/lang/nl/nav.php
+++ resources/lang/nl/nav.php
@@ -1,55 +1,19 @@
 <?php
 
-return array (
-  'account' => 
-  array (
-    'profile' => 'Mijn TakeDaily',
-    'deliveries' => 'Bestellingen',
-    'settings' => 'Klantgegevens',
-    'subscription' => 'Abonnement',
-    'billing' => 'Betaalmethode',
-    'logout' => 'Uitloggen',
-    'pick' => 'Vitamines',
-  ),
-  'items' => 
-  array (
-    'gifting' => 'Geef Takedaily cadeau',
-    'our-products' => 'Products'
-  ),
-  'subitems' => 
-  array (
-    'a-zink' => 
-    array (
-      'Mineralen' => 
-      array (
-        0 => 'zink',
-        7 => 'jod',
-        6 => 'kalium',
-        1 => 'selen',
-        10 => 'calium',
-        2 => 'mangan',
-        3 => 'magnesium',
-        9 => 'fosfor',
-        5 => 'kobber',
-        4 => 'krom',
-        8 => 'jern',
-      ),
-      'Vitamines' => 
-      array (
-        3 => 'vitamin-c',
-        4 => 'vitamin-a',
-        0 => 'vitamin-k',
-        1 => 'vitamin-e',
-        10 => 'vitamin-b6-pyridoxin',
-        2 => 'vitamin-d3',
-        5 => 'vitamin-b5-pantotensyre',
-        6 => 'vitamin-b3-niacin',
-        7 => 'vitamin-b2-riboflavin',
-        8 => 'vitamin-b1-tiamin',
-        9 => 'vitamin-b8-biotin',
-        11 => 'vitamin-b9-folatfolsyre',
-        12 => 'vitamin-b12-kobalamin',
-      ),
-    ),
-  ),
-);
+return [
+	'account'  =>
+		[
+			'profile'      => 'Mijn TakeDaily',
+			'deliveries'   => 'Bestellingen',
+			'settings'     => 'Klantgegevens',
+			'subscription' => 'Abonnement',
+			'billing'      => 'Betaalmethode',
+			'logout'       => 'Uitloggen',
+			'pick'         => 'Vitamines',
+		],
+	'items'    =>
+		[
+			'gifting'      => 'Geef Takedaily cadeau',
+			'our-products' => 'Products'
+		],
+];
