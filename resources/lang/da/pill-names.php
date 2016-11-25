d5ba5eefa3060a3166ddfc95a4ac3a903e6d3208
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 23:44:57 +0100

    Added pill names to translation

---
+++ resources/lang/da/pill-names.php
@@ -0,0 +1,18 @@
+<?php
+return [
+	'1a' => 'Multi Basic',
+	'1b' => 'Multi Vitamin D+',
+	'1c' => 'Multi Vitamin D+ Extra',
+	'2a' => 'Mom & Baby',
+	'2b' => 'Shape Me up',
+	'2c' => 'Energy Booster',
+	'2d' => 'Immune Booster',
+	'2e' => 'Bone & Muscle',
+	'3a' => 'I\'m not a veggie fan',
+	'3b' => 'I don\'t Eat Carbs',
+	'3c' => 'The Dairy Portion',
+	'3d' => 'I am Vegetarian / Vegan',
+	'3e' => 'Premium Omega-3 Fish Oil',
+	'3f' => 'Not a Butter Lover',
+	'3g' => 'Premium Omega-3 Chia Seed Oil'
+];
