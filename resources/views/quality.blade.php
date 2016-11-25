549a4301718852f4ea5d571797649c49b160011e
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 16:49:45 +0100

    Some quality styling

--- resources/views/quality.blade.php
+++ resources/views/quality.blade.php
@@ -16,16 +16,69 @@
 
 	<section class="quality_section_one">
 		<div class="container">
-			<div class="col-md-push-6">hej</div>
+			<div class="row">
+				<div class="aligner">
+					<div class="col-md-6 col-md-push-6">
+						<h2>Vores vitaminer og kosttilskud</h2>
+						<p>Alle vores vitaminer og kosttilskud er af bedste kvalitet. Alle anvendte grundstoffer og underleverandører er godkendt ned til mindste detalje i EU, og
+							vores
+							egne formulaer er i overensstemmelse med reglerne for kosttilskud udstukket af EFSA (European Food Safety Authority).</p>
+
+						<p>Uagtet hvilken sammensætning vi anbefaler dig, holder vi os altid under de daglige anbefalede værdier.</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 
 	<section class="quality_section_two">
+		<div class="container">
+			<div class="row">
+				<div class="aligner">
+					<div class="col-md-6">
+						<h2>Vores kapsler er 100% plantebaseret</h2>
+						<p>Som en af de eneste i Danmark, tilbyder vi ikke-gelatinebaserede kapsler i vores vitaminer og kosttilskud. Vores kapsler er lavet af det organiske stof
+							cellulose
+							(hydroxypropyl methylcellulose), som kommer fra træer. Det er en omstændig proces at producere, men det har flere gode egenskaber, blandt andet gør det
+							kroppens
+							optagelse af vitaminer, mineraler og omega-3 fedtsyrer væsentligt hurtigere. Desuden er kapslerne egnet til folk, som ikke ønsker gelatinebaserede
+							produkter
+							pga. livsstil eller religion.</p>
 
+						<strong>I øvrigt er vores vitaminer, mineraler og omega-kapsler:</strong>
+
+						<ul>
+							<li>Certificeret i henhold til ISO 9001:2008 og GMP (Good Manufactoring Practice)</li>
+							<li>100% uden konserveringsmidler.</li>
+							<li>Godkendt til Kosher/Halal samt vegetarer/veganere</li>
+							<li>Ikke-allergifremmende</li>
+							<li>GMO-fri (ikke gen-modificeret organisme)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 
 	<section class="quality_section_three">
+		<div class="container">
+			<div class="row">
+				<div class="aligner">
+					<div class="col-md-6 text-center">
+						<img src="/images/foa_logo.png" alt="Friends of the Sea"/>
+					</div>
 
+					<div class="col-md-6">
+						<h2>Vores OMEGA-3</h2>
+						<p>Vores omega-3 fiskeolie er baseret på den reneste olie fra anjoser og sardiner og indeholder over 55% EPA & DHA fedtsyrer.</p>
+
+						<p>Vores fiskeolie er underlagt de strengeste kvalitetskrav, og er certificeret gennem hele processen fra produktion til forsendelse. Alle vores
+							produktioner
+							kan spores tilbage til hver enkelt fangst af fisk, som er certificeret af ”Friends of the Sea”. Dette sikrer at miljø og bæredygtighed er i højsæde.</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 
 
@@ -54,16 +107,29 @@
 		}
 
 		section.quality_section_one {
-			background-image: linear-gradient(-90deg, rgba(255,255,255, 1) 0%, rgba(255,255,255, 0.00) 100%), linear-gradient(-90deg, rgba(255,255,255, 1) 0%, rgba(255,255,255, 0.00) 100%), url(/images/quality/one.jpg);
+			background-image: linear-gradient(-90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.00) 100%), linear-gradient(-90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.00) 100%), url(/images/quality/one.jpg);
 			background-position: left center;
 		}
 
 		section.quality_section_two {
-			background-image: linear-gradient(-270deg, rgba(255,255,255, 1) 0%, rgba(255,255,255, 0.00) 100%), linear-gradient(-270deg, rgba(255,255,255, 1) 0%, rgba(255,255,255, 0.00) 100%), url(/images/quality/two.jpg);
+			background-image: linear-gradient(-270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.00) 100%), linear-gradient(-270deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.00) 100%), url(/images/quality/two.jpg);
 			background-position: right center;
 		}
 
-		section.quality_section_three {
+		section .aligner {
+			display: flex;
+			align-items: center;
+		}
+
+		@media all and (max-width: 991px)
+		{
+			section {
+				background-size: cover;
+			}
+
+			section .aligner {
+				display: block;
+			}
 		}
 	</style>
 @endsection
