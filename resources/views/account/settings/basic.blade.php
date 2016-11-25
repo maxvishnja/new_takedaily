0e9adcaf2ec1f4ba47c811c8152ea79b0b5616bc
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 14:42:22 +0100

    Moved some input fields in basic settings

--- resources/views/account/settings/basic.blade.php
+++ resources/views/account/settings/basic.blade.php
@@ -8,6 +8,16 @@
 	<h1>{{ trans('account.settings_basic.header') }}</h1>
 
 	<form method="post" class="account-form">
+		<label style="display: block;">
+			<span>{{ trans("attributes.name") }}</span>
+			<input type="text" id="name" class="input input--full input--regular" name="name" data-original="{{ $customer->getName() }}" value="{{ Request::old('name', $customer->getName()) }}" required="required" aria-required="true"/>
+		</label>
+
+		<label style="display: block;">
+			<span>{{ trans("attributes.email") }}</span>
+			<input type="text" id="email" class="input input--full input--regular" name="email" data-original="{{ $customer->getUser()->email }}" value="{{ Request::old('email', $customer->getUser()->email) }}" required="required" aria-required="true"/>
+		</label>
+
 		@foreach($attributes as $attribute)
 			<label style="display: block;">
 				<span>{{ trans("attributes.{$attribute->identifier}") }}</span>
@@ -22,16 +32,6 @@
 				@endif
 			</label>
 		@endforeach
-
-		<label style="display: block;">
-			<span>{{ trans("attributes.email") }}</span>
-			<input type="text" id="email" class="input input--full input--regular" name="email" data-original="{{ $customer->getUser()->email }}" value="{{ Request::old('email', $customer->getUser()->email) }}" required="required" aria-required="true"/>
-		</label>
-
-		<label style="display: block;">
-			<span>{{ trans("attributes.name") }}</span>
-			<input type="text" id="name" class="input input--full input--regular" name="name" data-original="{{ $customer->getName() }}" value="{{ Request::old('name', $customer->getName()) }}" required="required" aria-required="true"/>
-		</label>
 
 		<label style="display: block;">
 			<span>{{ trans("attributes.new_password") }}</span>
