2bafed67e4ab76f9caaa371e89def4164368d877
Lasse Rafn <lasserafn@gmail.com>
2016-11-25 17:55:03 +0100

    Some cool pick package stuff

--- resources/lang/da/pick-package.php
+++ resources/lang/da/pick-package.php
@@ -2,8 +2,9 @@
 
 return [
 	'title'       => 'Vælg din TakeDaily pakke',
-	'sub-title'       => 'Du vil, afhængig af din pakke, blive spurgt om yderligere oplysninger efterfølgende',
+	'sub-title'   => 'Du vil, afhængig af din pakke, blive spurgt om yderligere oplysninger efterfølgende',
 	'select-btn'  => 'Vælg denne',
+	'read-btn'    => 'Læs mere',
 	'custom-pill' => 'Vælg en pille',
 	'pills'       =>
 		[
@@ -25,65 +26,65 @@ return [
 				[
 					'name'        => 'Busy life/Energy',
 					'description' => 'activeert je natuurlijke energie in het lichaam. kan het gebrek aan energie helpen verminderen.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'relax'              =>
 				[
 					'name'        => 'Relax',
 					'description' => 'Activeert de natuurlijke energie in het lichaam. Helpt bij vermoeidheid/moeheid. Goed voor de gemoedstoestand.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'sportsman_woman'    =>
 				[
 					'name'        => 'Sportsman/-woman',
 					'description' => 'Activeert je natuurlijke energie in het lichaam. speelt een rol bij het behouden van sterke spieren. bevordert de weerstand tijdens en na fysieke inspanning.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'balanced_body'      =>
 				[
 					'name'        => 'Body in Balance',
 					'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'immunity_boost'     =>
 				[
 					'name'        => 'Immune Booster',
 					'description' => 'zorgt mede voor een goede weerstand. helpt het immuunsysteem.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'vega_power'         =>
 				[
 					'name'        => 'Vega Power',
 					'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'women_menopause'    =>
 				[
 					'name'        => 'Women in Menopause',
 					'description' => 'Calcium en vitamine D helpen het verlies van botmineralen bij vrouwen in de meno-pauze te verminderen.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'heart_bloodvessels' =>
 				[
 					'name'        => 'Hart & Bloedvaten',
 					'description' => 'DHA en EPA dragen bij tot de instandhouding van de normale bloeddruk. draagt bij tot de normale werking van het hart.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'beauty_boost'       =>
 				[
 					'name'        => 'Beauty Boost',
 					'description' => 'Houdt je haar sterk voor het behoud van glanzend haar. goed voor nagels draagt bij aan het behoud van normale nagels. helpt bij de verzorging van de huid van binnenuit.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 			'pregnant'           =>
 				[
 					'name'        => 'Pregnant woman & Pregnant to be',
 					'description' => 'Foliumzuur draagt bij tot de groei van het ongeboren kind tijdens de zwangerschap. De inname van docosahexaeenzuur (DHA) door de moeder draagt bij tot normale ontwikkeling van de hersenen en ogen bij het ongeboren kind.',
-					'icons'       => ['blood']
+					'icons'       => [ 'blood' ]
 				],
 		],
-    'errors' => [
-    	'not-found' => 'Package not found.'
-    ],
-    'updated' => 'Din pakke blev opdateret!'
+	'errors'      => [
+		'not-found' => 'Package not found.'
+	],
+	'updated'     => 'Din pakke blev opdateret!'
 ];
