8dcd5041c4cb9b704d4ff5d2076eb019e2b45d67
Lasse Rafn <lasserafn@gmail.com>
2016-11-26 14:15:33 +0100

    Fix nav

--- resources/lang/da/nav.php
+++ resources/lang/da/nav.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 return array (
   'account' => 
   array (
@@ -19,96 +18,4 @@ return array (
     'about-us' => 'Om os',
     'how-it-works' => 'Sådan virker det',
   ),
-  'subitems' => 
-  array (
-    'a-zink' => 
-    array (
-      'Mineraler' => 
-      array (
-        0 => 'zink',
-        1 => 'selen',
-        2 => 'mangan',
-        3 => 'magnesium',
-        4 => 'krom',
-        5 => 'kobber',
-        6 => 'kalium',
-        7 => 'jod',
-        8 => 'jern',
-        9 => 'fosfor',
-        10 => 'calcium',
-      ),
-      'Vitaminer' => 
-      array (
-        0 => 'vitamin-k',
-        1 => 'vitamin-e',
-        2 => 'vitamin-d3',
-        3 => 'vitamin-c',
-        4 => 'vitamin-a',
-        5 => 'vitamin-b5-pantotensyre',
-        6 => 'vitamin-b3-niacin',
-        7 => 'vitamin-b2-riboflavin',
-        8 => 'vitamin-b1-tiamin',
-        9 => 'vitamin-b8-biotin',
-        10 => 'vitamin-b6-pyridoxin',
-        11 => 'vitamin-b9-folatfolsyre',
-        12 => 'vitamin-b12-kobalamin',
-      ),
-    ),
-  ),
 );
-=======
-return [
-	'account'  =>
-		[
-			'profile'      => 'Mit TakeDaily',
-			'deliveries'   => 'Leveringer',
-			'settings'     => 'Indstillinger',
-			'subscription' => 'Abonnent',
-			'billing'      => 'Fakturering',
-			'logout'       => 'Log ud',
-			'pick'         => 'Vitaminer',
-		],
-	'items'    =>
-		[
-			'gifting'      => 'Giv i gave',
-			'our-products' => 'Produkter',
-			'about-us'     => 'Om os',
-		],
-	'subitems' =>
-		[
-			'a-zink' =>
-				[
-					'Mineraler' =>
-						[
-							0  => 'zink',
-							1  => 'selen',
-							2  => 'mangan',
-							3  => 'magnesium',
-							4  => 'krom',
-							5  => 'kobber',
-							6  => 'kalium',
-							7  => 'jod',
-							8  => 'jern',
-							9  => 'fosfor',
-							10 => 'calcium',
-						],
-					'Vitaminer' =>
-						[
-							0  => 'vitamin-k',
-							1  => 'vitamin-e',
-							2  => 'vitamin-d3',
-							3  => 'vitamin-c',
-							4  => 'vitamin-a',
-							5  => 'vitamin-b5-pantotensyre',
-							6  => 'vitamin-b3-niacin',
-							7  => 'vitamin-b2-riboflavin',
-							8  => 'vitamin-b1-tiamin',
-							9  => 'vitamin-b8-biotin',
-							10 => 'vitamin-b6-pyridoxin',
-							11 => 'vitamin-b9-folatfolsyre',
-							12 => 'vitamin-b12-kobalamin',
-						],
-				],
-		],
-];
->>>>>>> 3ebd9a70e11bc116394aac8c804c76cf7872dafe
