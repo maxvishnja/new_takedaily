cbf94c49f0b6a0c6ddf472b49086c526295da834
Lasse Rafn <lasserafn@gmail.com>
2016-12-04 20:35:30 +0100

    Fix

--- resources/views/flow-label.blade.php
+++ resources/views/flow-label.blade.php
@@ -30,9 +30,11 @@
 				<div class="m-t-30 m-b-10"><a href="#" class="readMoreBtn">{{ trans('flow-actions.read-more') }}</a></div>
 				<div class="description">
 					@if(isset($descriptions[$vitamin])) <p>{!! $descriptions[$vitamin] !!}</p> @endif
-					<div class="vitamin_advantage_list">
-						{!! trans('label-' . strtolower($vitamin) . '.web_advantage_list') !!}
-					</div>
+					@if(trans('label-' . strtolower($vitamin) . '.web_advantage_list') != 'label-' . strtolower($vitamin) . '.web_advantage_list')
+						<div class="vitamin_advantage_list">
+							{!! trans('label-' . strtolower($vitamin) . '.web_advantage_list') !!}
+						</div>
+					@endif
 
 					<div class="m-t-20 m-b-10"><a href="#" class="seeIngredientsBtn">{{ trans('flow-actions.see-ingredients') }}</a></div>
 					<div class="ingredients">@include('flow-includes.views.vitamin_table', ['label' => strtolower($vitamin)])</div>
