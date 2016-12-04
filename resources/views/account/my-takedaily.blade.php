cc8df9135c00a6e74111f1ea30c0481f10d85f6a
Lasse Rafn <lasserafn@gmail.com>
2016-12-04 17:19:58 +0100

    Oversigt på takedaily profil

--- resources/views/account/my-takedaily.blade.php
+++ resources/views/account/my-takedaily.blade.php
@@ -8,7 +8,22 @@
 	<h1>{{ trans('account.home.header') }}</h1>
 
 	@foreach(Auth::user()->getCustomer()->getVitaminModels() as $vitamin)
-		<span class="icon pill-{{ $vitamin->code }}"></span>
+		<div class="new_vitamin_item">
+
+			<div class="pill_section">
+				<span class="icon pill-{{ strtolower($vitamin->code) }}"></span>
+			</div>
+
+			<div class="content_section">
+				<strong>
+					{{ $vitamin->name }}
+				</strong>
+				<p>{!! trans("label-{$vitamin->code}.web_description") !!}</p>
+			</div>
+		</div>
 	@endforeach
+
+
+
 	<div class="m-t-10"><a href="/flow" class="button button--green">{{ trans('account.home.button-change') }}</a></div>
 @endsection
