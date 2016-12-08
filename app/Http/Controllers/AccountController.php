f468c6de6f03a5d28a5cc3ba20532c87190becd2
Lasse Rafn <lasserafn@gmail.com>
2016-12-08 11:58:26 +0100

    Some minor stuff to account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -22,40 +22,42 @@ class AccountController extends Controller
 
 	function __construct()
 	{
-		$this->middleware('user');
+		$this->middleware( 'user' );
 		$this->user = \Auth::user();
 		if ( $this->user && $this->user->getCustomer() )
 		{
 			$this->customer = $this->user->getCustomer();
-			\View::share('customer', $this->customer);
+			\View::share( 'customer', $this->customer );
 		}
 
 	}
 
 	function getHome()
 	{
-		return view('account.my-takedaily');
+		$orders = $this->customer->getOrders();
+
+		return view( 'account.my-takedaily', compact( 'orders' ) );
 	}
 
-	function updatePreferences(Request $request)
+	function updatePreferences( Request $request )
 	{
-		if( !$this->customer || $this->customer->plan->isCustom() )
+		if ( ! $this->customer || $this->customer->plan->isCustom() )
 		{
-			return redirect()->back()->withErrors(trans('account.general.errors.custom-package-cant-change'));
+			return redirect()->back()->withErrors( trans( 'account.general.errors.custom-package-cant-change' ) );
 		}
 
-		return redirect()->route('flow');
+		return redirect()->route( 'flow' );
 	}
 
 	function updateVitamins()
 	{
 		$combinations = $this->user->getCustomer()->calculateCombinations();
-		$vitamins     = [ ];
+		$vitamins     = [];
 
 		foreach ( $combinations as $key => $combination )
 		{
-			$pill = PillLibrary::getPill($key, $combination);
-			$vitamin = Vitamin::select('id')->whereCode($pill)->first();
+			$pill    = PillLibrary::getPill( $key, $combination );
+			$vitamin = Vitamin::select( 'id' )->whereCode( $pill )->first();
 
 			if ( $vitamin )
 			{
@@ -63,89 +65,89 @@ class AccountController extends Controller
 			}
 		}
 
-		$this->user->getCustomer()->getPlan()->update([
-			'vitamins' => json_encode($vitamins)
-		]);
+		$this->user->getCustomer()->getPlan()->update( [
+			'vitamins' => json_encode( $vitamins )
+		] );
 
-		return \Redirect::action('AccountController@getSettingsSubscription')->with('success', trans('account.general.successes.vitamins-updated'));
+		return \Redirect::action( 'AccountController@getSettingsSubscription' )->with( 'success', trans( 'account.general.successes.vitamins-updated' ) );
 	}
 
 	function getTransactions()
 	{
-		return view('account.transactions', [
+		return view( 'account.transactions', [
 			'orders' => $this->customer->getOrders()
-		]);
+		] );
 	}
 
-	function getTransaction($id)
+	function getTransaction( $id )
 	{
-		$order = $this->customer->getOrderById($id);
+		$order = $this->customer->getOrderById( $id );
 
-		if ( !$order )
+		if ( ! $order )
 		{
-			return \Redirect::to('/account/transactions')->withErrors(trans('messages.errors.transactions.not_found'));
+			return \Redirect::to( '/account/transactions' )->withErrors( trans( 'messages.errors.transactions.not_found' ) );
 		}
 
-		return view('account.transaction', [
+		return view( 'account.transaction', [
 			'order' => $order
-		]);
+		] );
 	}
 
 	function getSettingsBilling()
 	{
 		$sources = $this->customer->getPaymentMethods();
 
-		return view('account.settings.billing', [
+		return view( 'account.settings.billing', [
 			'sources' => $sources['methods'],
-		    'method' => $sources['type']
-		]);
+			'method'  => $sources['type']
+		] );
 	}
 
 	function getSettingsBillingRemove()
 	{
-		if ( !$this->customer->removePaymentOption() )
+		if ( ! $this->customer->removePaymentOption() )
 		{
-			return redirect()->action('AccountController@getSettingsBilling')->withErrors(trans('messages.successes.billing.removing-failed'));
+			return redirect()->action( 'AccountController@getSettingsBilling' )->withErrors( trans( 'messages.successes.billing.removing-failed' ) );
 		}
 
-		return redirect()->action('AccountController@getSettingsBilling')->with('success', trans('messages.successes.billing.removed'));
+		return redirect()->action( 'AccountController@getSettingsBilling' )->with( 'success', trans( 'messages.successes.billing.removed' ) );
 	}
 
 	function getSettingsBillingAdd()
 	{
-		if ( count($this->customer->getPaymentMethods()['methods']) > 0 )
+		if ( count( $this->customer->getPaymentMethods()['methods'] ) > 0 )
 		{
-			return \Redirect::action('AccountController@getSettingsBilling');
+			return \Redirect::action( 'AccountController@getSettingsBilling' );
 		}
 
-		return view('account.settings.billing-add');
+		return view( 'account.settings.billing-add' );
 	}
 
-	function postSettingsBillingAdd(Request $request)
+	function postSettingsBillingAdd( Request $request )
 	{
 		$user = \Auth::user();
 
-		if ( count($this->customer->getPaymentMethods()['methods']) == 0 )
+		if ( count( $this->customer->getPaymentMethods()['methods'] ) == 0 )
 		{
 			try
 			{
-				$stripeCustomer = Customer::create([
+				$stripeCustomer = Customer::create( [
 					"description" => "Customer for {$user->getEmail()}",
-					"source"      => $request->get('stripeToken')
-				]);
+					"source"      => $request->get( 'stripeToken' )
+				] );
 
-				$user->getCustomer()->getPlan()->update([
+				$user->getCustomer()->getPlan()->update( [
 					'stripe_token' => $stripeCustomer->id
-				]);
-			} catch( Card $ex )
+				] );
+			} catch ( Card $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
-			} catch( \Exception $ex )
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
+			} catch ( \Exception $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
-			} catch( \Error $ex )
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
+			} catch ( \Error $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
 			}
 		}
 		else
@@ -153,83 +155,85 @@ class AccountController extends Controller
 			$stripeCustomer = $user->getCustomer()->getStripeCustomer();
 			try
 			{
-				$stripeCustomer->sources->create([
-					'source' => $request->get('stripeToken')
-				]);
-			} catch( Card $ex )
+				$stripeCustomer->sources->create( [
+					'source' => $request->get( 'stripeToken' )
+				] );
+			} catch ( Card $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
-			} catch( \Exception $ex )
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
+			} catch ( \Exception $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
-			} catch( \Error $ex )
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
+			} catch ( \Error $ex )
 			{
-				return \Redirect::back()->withErrors([ trans('checkout.messages.payment-error', [ 'error' => $ex->getMessage() ]) ])->withInput();
+				return \Redirect::back()->withErrors( [ trans( 'checkout.messages.payment-error', [ 'error' => $ex->getMessage() ] ) ] )->withInput();
 			}
 		}
 
-		return \Redirect::action('AccountController@getSettingsBilling')->with('success', trans('checkout.messages.success.card-added'));
+		return \Redirect::action( 'AccountController@getSettingsBilling' )->with( 'success', trans( 'checkout.messages.success.card-added' ) );
 	}
 
 	function getSettingsBasic()
 	{
-		return view('account.settings.basic', [
-			'attributes' => $this->customer->getCustomerAttributes(true)
-		]);
+		return view( 'account.settings.basic', [
+			'attributes' => $this->customer->getCustomerAttributes( true )
+		] );
 	}
 
-	function postSettingsBasic(Request $request)
+	function postSettingsBasic( Request $request )
 	{
-		$this->validate($request, [
+		$this->validate( $request, [
 			'email'    => 'required|email|unique:users,email,' . $this->user->id,
 			'name'     => 'required',
 			'password' => 'confirmed'
-		]);
+		] );
 
-		foreach ( $request->input('attributes') as $attributeId => $attributeValue )
+		foreach ( $request->input( 'attributes' ) as $attributeId => $attributeValue )
 		{
-			$this->customer->customerAttributes()->where('id', $attributeId)->update([ 'value' => $attributeValue ]);
+			$this->customer->customerAttributes()->where( 'id', $attributeId )->update( [ 'value' => $attributeValue ] );
 		}
 
-		$this->customer->birthday          = $request->get('birthday');
-		$this->customer->accept_newletters = $request->get('newsletters', 0);
-		$this->customer->gender            = $request->get('gender', 'Male');
-		$this->user->email                 = $request->get('email');
-		$this->user->name                  = $request->get('name');
+		$this->customer->birthday          = $request->get( 'birthday' );
+		$this->customer->accept_newletters = $request->get( 'newsletters', 0 );
+		$this->customer->gender            = $request->get( 'gender', 'Male' );
+		$this->user->email                 = $request->get( 'email' );
+		$this->user->name                  = $request->get( 'name' );
 
-		if ( $request->get('password') != '' )
+		if ( $request->get( 'password' ) != '' )
 		{
-			$this->user->password = bcrypt($request->get('password'));
+			$this->user->password = bcrypt( $request->get( 'password' ) );
 		}
 
 		$this->customer->save();
 		$this->user->save();
 
-		return \Redirect::to('/account/settings/basic')->with('success', trans('messages.successes.profile.updated'));
+		return \Redirect::to( '/account/settings/basic' )->with( 'success', trans( 'messages.successes.profile.updated' ) );
 	}
 
 	function getSettingsSubscription()
 	{
-		return view('account.settings.subscription', [
+		return view( 'account.settings.subscription', [
 			'plan' => $this->customer->getPlan()
-		]);
+		] );
 	}
 
-	function postSettingsSubscriptionSnooze(Request $request)
+	function postSettingsSubscriptionSnooze( Request $request )
 	{
-		if ( !$this->customer->getPlan()->isSnoozeable() )
+		if ( ! $this->customer->getPlan()->isSnoozeable() )
 		{
-			return redirect()->action('AccountController@getSettingsSubscription')->withErrors(trans('messages.errors.subscription.not-snoozed'));
+			return redirect()->action( 'AccountController@getSettingsSubscription' )->withErrors( trans( 'messages.errors.subscription.not-snoozed' ) );
 		}
 
-		if( $request->get('days', 7) > 28 )
+		if ( $request->get( 'days', 7 ) > 28 )
 		{
-			return redirect()->back()->withErrors(trans('account.general.errors.max-snooze'));
+			return redirect()->back()->withErrors( trans( 'account.general.errors.max-snooze' ) );
 		}
 
-		$this->customer->getPlan()->snooze($request->get('days', 7));
+		$this->customer->getPlan()->snooze( $request->get( 'days', 7 ) );
 
-		return redirect()->action('AccountController@getSettingsSubscription')->with('success', trans('messages.successes.subscription.snoozed', [ 'days' => $request->get('days', 7) ]));
+		return redirect()
+			->action( 'AccountController@getSettingsSubscription' )
+			->with( 'success', trans( 'messages.successes.subscription.snoozed', [ 'days' => $request->get( 'days', 7 ) ] ) );
 	}
 
 	function getSettingsSubscriptionStart()
@@ -241,7 +245,7 @@ class AccountController extends Controller
 
 		$this->customer->getPlan()->start();
 
-		return redirect()->action('AccountController@getSettingsSubscription')->with('success', trans('messages.successes.subscription.started'));
+		return redirect()->action( 'AccountController@getSettingsSubscription' )->with( 'success', trans( 'messages.successes.subscription.started' ) );
 	}
 
 	function getSettingsSubscriptionRestart()
@@ -253,19 +257,19 @@ class AccountController extends Controller
 
 		$this->customer->getPlan()->startFromToday();
 
-		return redirect()->action('AccountController@getSettingsSubscription')->with('success', trans('messages.successes.subscription.started'));
+		return redirect()->action( 'AccountController@getSettingsSubscription' )->with( 'success', trans( 'messages.successes.subscription.started' ) );
 	}
 
 	function getSettingsSubscriptionCancel()
 	{
-		if ( !$this->customer->getPlan()->isCancelable() )
+		if ( ! $this->customer->getPlan()->isCancelable() )
 		{
 			return \Redirect::back();
 		}
 
 		$this->customer->getPlan()->cancel();
 
-		return redirect()->action('AccountController@getSettingsSubscription')->with('success', trans('messages.successes.subscription.cancelled'));
+		return redirect()->action( 'AccountController@getSettingsSubscription' )->with( 'success', trans( 'messages.successes.subscription.cancelled' ) );
 	}
-	
+
 }
--- resources/views/account/my-takedaily.blade.php
+++ resources/views/account/my-takedaily.blade.php
@@ -23,7 +23,32 @@
 		</div>
 	@endforeach
 
-
-
 	<div class="m-t-10"><a href="/flow" class="button button--green">{{ trans('account.home.button-change') }}</a></div>
+
+	@if($orders->count() > 0 )
+		<hr>
+		<h1>{{ trans('account.transactions.header') }}</h1>
+		<table class="table table--full table--striped text-left table--responsive">
+			<thead>
+			<tr>
+				<th>#</th>
+				<th>{{ trans('account.transactions.table.date') }}</th>
+				<th>{{ trans('account.transactions.table.amount') }}</th>
+				<th>{{ trans('account.transactions.table.status') }}</th>
+				<th></th>
+			</tr>
+			</thead>
+			<tbody>
+			@foreach($orders as $order)
+				<tr>
+					<td data-th="#">#{{ $order->getPaddedId() }}</td>
+					<td data-th="{{ trans('account.transactions.table.date') }}">{{ \Jenssegers\Date\Date::createFromFormat('Y-m-d H:i:s', $order->created_at)->format('j. M Y H:i') }}</td>
+					<td data-th="{{ trans('account.transactions.table.amount') }}"><strong>{{ trans('general.money-fixed-currency', ['amount' => \App\Apricot\Libraries\MoneyLibrary::toMoneyFormat($order->getTotal(), true), 'currency' => $order->currency]) }}</strong></td>
+					<td data-th="{{ trans('account.transactions.table.status') }}"><span class="state-label state-label--{{ $order->state  }}">{{ trans("order.state.{$order->state}") }}</span></td>
+					<td data-th="&nbsp;"><a href="{{URL::action('AccountController@getTransaction', [ 'id' => $order->id ]) }}" class="button button--small button--rounded button--grey">{{ trans('account.transactions.button-show-text') }}</a></td>
+				</tr>
+			@endforeach
+			</tbody>
+		</table>
+	@endif
 @endsection
