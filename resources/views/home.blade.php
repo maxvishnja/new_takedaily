349a41e1ca35db064edeba1f8aeadd636ca3cb53
Lasse Rafn <lasserafn@gmail.com>
2016-12-13 22:38:18 +0100

    Trans

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,8 +28,7 @@
 							<div class="row">
 								<div class="col-md-8">
 									<h1>{!! trans('home.header.title-1') !!}</h1>
-									<p style="font-size: 18px;"><strong>Med TakeDaily kan du hver dag supplere din kost med præcis de vitaminer og mineraler, du har brug
-											for</strong></p>
+									<p style="font-size: 18px;"><strong>{{ trans('home.header.subtitle') }}</strong></p>
 									<a href="{{ url()->route('flow') }}"
 									   class="button button--rounded button--huge button--landing button--green m-t-30">
 										<strong>{!! trans('home.header.button-click-here') !!}</strong>
