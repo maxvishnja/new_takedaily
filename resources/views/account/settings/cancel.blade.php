59bbc9b4259922df51cba555ca77c658215793cc
Lasse Rafn <lasserafn@gmail.com>
2016-12-13 20:47:11 +0100

    Minor stuff

--- resources/views/account/settings/cancel.blade.php
+++ resources/views/account/settings/cancel.blade.php
@@ -8,15 +8,15 @@
 	<h1>{!! trans('account.settings_cancel.header') !!}</h1>
 
 	<form action="{{ url()->action('AccountController@getSettingsSubscriptionCancel') }}" id="form" method="post">
-		<label for="reason">{{ trans('account.settings_cancel.question') }}</label>
-		<select name="reason" id="reason">
+		<label for="reason">{{ trans('account.settings_cancel.question') }}</label><br/>
+		<select name="reason" class="select select--regular" id="reason">
 			<option value="{{ trans('account.settings_cancel.reasons.0') }}">{{ trans('account.settings_cancel.reasons.0') }}</option>
 			<option value="{{ trans('account.settings_cancel.reasons.1') }}">{{ trans('account.settings_cancel.reasons.1') }}</option>
 			<option value="{{ trans('account.settings_cancel.reasons.2') }}">{{ trans('account.settings_cancel.reasons.2') }}</option>
 			<option value="{{ trans('account.settings_cancel.reasons.3') }}">{{ trans('account.settings_cancel.reasons.3') }}</option>
 			<option value="{{ trans('account.settings_cancel.reasons.4') }}">{{ trans('account.settings_cancel.reasons.4') }}</option>
 			<option value="{{ trans('account.settings_cancel.reasons.5') }}">{{ trans('account.settings_cancel.reasons.5') }}</option>
-		</select>
+		</select><br/><br/>
 		<button type="submit" class="button button--green button--large">{{ trans('account.settings_cancel.cancel') }}</button>
 		<button type="submit" class="button button--white button--text-green button--large">{{ trans('account.settings_cancel.submit') }}</button>
 	</form>
