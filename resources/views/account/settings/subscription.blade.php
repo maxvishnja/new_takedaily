7612065b19a54772a06395c6009bddd590ba9c9e
Lasse Rafn <lasserafn@gmail.com>
2016-12-13 22:06:20 +0100

    Fix

--- resources/views/account/settings/subscription.blade.php
+++ resources/views/account/settings/subscription.blade.php
@@ -9,7 +9,7 @@
 	<h2>{!! trans('account.settings_subscription.total', [ 'amount' => trans('general.money-fixed-currency', ['amount' => \App\Apricot\Libraries\MoneyLibrary::toMoneyFormat($plan->getTotal(), true), 'currency' => $plan->currency])]) !!}</h2>
 
 	@if( $plan->isActive() )
-		<p>{!! trans('account.settings_subscription.next-date', ['date' => Date::createFromFormat('Y-m-d H:i:s', $plan->getRebillAt())->format('j. M Y') ]) !!}</p>
+		<p>{!! strip_tags(trans('account.settings_subscription.next-date', ['date' => Date::createFromFormat('Y-m-d H:i:s', $plan->getRebillAt())->format('j. M Y') ]), '<strong>') !!}</p>
 
 		<div class="m-b-30">
 			@if($plan->isCancelable())
