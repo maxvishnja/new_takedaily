899c443cefbc712acd8f8889c6e633cdff03efba
Lasse Rafn <lasserafn@gmail.com>
2017-01-04 01:19:06 +0100

    CSRF fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,9 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         '/dashboard/upload/image',
-		'/checkout/mollie'
+		'/checkout/mollie',
+        '/cart-pick-n-mix',
+        '/save-state',
+        '/flow/recommendations',
     ];
 }
