ec23b902f61d850dc6ea18530e5f4c842ad8cda2
Lasse Rafn <lasserafn@gmail.com>
2017-01-05 14:12:47 +0100

    Fixes

--- resources/views/pdf/sticker.blade.php
+++ resources/views/pdf/sticker.blade.php
@@ -160,7 +160,6 @@
 
 	<div style="font-weight: 200;font-size: 6.5pt;color: #1A8562;line-height: 1;position: absolute; bottom: 4mm; text-align: center">
 		{!! trans('label-product.Use') !!} {!! trans('label-product.Store') !!}<br/>
-		{!! trans('label-product.production') !!}<br/>
 		<div>{{ trans('label-product.address') }}</div>
 		<strong>{{ trans('label-product.Questions') }}</strong>
 	</div>
