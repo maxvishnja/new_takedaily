98bda411fe5801c7ac65685757607e3f0f1fb1b4
Lasse Rafn <lasserafn@gmail.com>
2017-01-12 14:55:44 +0100

    Ignored /flow in CSRF verification aswell. One user got an error, and considering its barely a security-risk, I decided its not worth the risk of losing customers.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,18 +6,19 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        '/dashboard/upload/image',
+	/**
+	 * The URIs that should be excluded from CSRF verification.
+	 *
+	 * @var array
+	 */
+	protected $except = [
+		'/dashboard/upload/image',
 		'/checkout/mollie',
-        '/cart-pick-n-mix',
-        '/save-state',
-        '/flow/recommendations',
-        '/checkout/apply-coupon',
-        '/pick-n-mix'
-    ];
+		'/cart-pick-n-mix',
+		'/save-state',
+		'/flow/recommendations',
+		'/checkout/apply-coupon',
+		'/pick-n-mix',
+		'/flow'
+	];
 }
