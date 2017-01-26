c45b40f14830224a875658ee6ae4b01619ae8531
Max <adm@Mac-Pro-Aleksandr.local>
2017-01-26 17:14:38 +0300

    add GA pixels

--- resources/views/includes/app/header.blade.php
+++ resources/views/includes/app/header.blade.php
@@ -32,6 +32,17 @@
 	<script src="/js/html5shiv.min.js"></script>
     <script src="/js/modernizr-custom.min.js"></script>
 	<![endif]-->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', '{{ trans('general.google_analytics_property') }}', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+
 </head>
 
 <body class="@yield('pageClass', 'index')">
