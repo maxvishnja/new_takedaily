e36ad0b9316ca45d2bb3325ca969ea02e78168f2
Max <adm@Mac-Pro-Aleksandr.local>
2017-01-31 14:59:16 +0300

    fix cupons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
 	<title>TakeDaily Admin</title>
 	<link type="text/css" href="/admin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="/admin/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link type="text/css" href="/admin/bootstrap/css/bootstrap-datepicker.css" rel="stylesheet">
+	{{--<link type="text/css" href="/admin/bootstrap/css/bootstrap-datepicker.css" rel="stylesheet">--}}
 	<link type="text/css" href="/admin/css/theme.css" rel="stylesheet">
 	<link type="text/css" href="/admin/images/icons/css/font-awesome.css" rel="stylesheet">
 	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
@@ -48,7 +48,7 @@
 <script src="/admin/js/jquery-1.9.1.min.js" type="text/javascript"></script>
 <script src="/admin/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
 <script src="/admin/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="/admin/bootstrap/js/bootstrap-datepicker.js" type="text/javascript"></script>
+{{--<script src="/admin/bootstrap/js/bootstrap-datepicker.js" type="text/javascript"></script>--}}
 <script src="/admin/js/flot/jquery.flot.js" type="text/javascript"></script>
 <script src="/admin/js/flot/jquery.flot.resize.js" type="text/javascript"></script>
 <script src="/admin/js/datatables/jquery.dataTables.js" type="text/javascript"></script>
