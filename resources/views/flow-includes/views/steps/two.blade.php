7f20a64f4a7d4d5c7d737164d0db30e3d936ef6e
Max <adm@Mac-Pro-Aleksandr.local>
2017-03-10 15:03:58 +0300

    add new question

--- resources/views/flow-includes/views/steps/two.blade.php
+++ resources/views/flow-includes/views/steps/two.blade.php
@@ -245,7 +245,7 @@
 
 	@if(App::getLocale() == "nl")
 
-	<div data-sub-step="11" class="sub_step" v-bind:class="{ 'sub_step--skip': user_data.pregnant == 2 || user_data.diet == 1 || user_data.smokes == 1
+	<div data-sub-step="11" class="sub_step" v-bind:class="{ 'sub_step--skip': user_data.pregnant == 1 || user_data.diet == 1 || user_data.smokes == 1
 															|| user_data.sports == 4 || user_data.stressed == 1 || user_data.immune_system == 2  || user_data.lacks_energy == 2
 															|| user_data.joints == 2}">
 		<div class="count-step">(11/11)</div>
