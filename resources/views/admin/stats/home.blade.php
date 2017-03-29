7936024833ab5a92d81050b8c499ce125ec0a316
Max <adm@Mac-Pro-Aleksandr.local>
2017-03-29 17:17:02 +0300

    add new legend

--- resources/views/admin/stats/home.blade.php
+++ resources/views/admin/stats/home.blade.php
@@ -164,12 +164,9 @@
                         allowPointSelect: true,
                         cursor: 'pointer',
                         dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                            }
-                        }
+                            enabled: false
+                        },
+                        showInLegend: true
                     }
                 },
                 series: [{
