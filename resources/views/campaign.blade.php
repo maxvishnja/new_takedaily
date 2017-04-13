18257cb3bcd150c1bb2ec91eec982bc349dd7318
Max <adm@Mac-Pro-Aleksandr.local>
2017-04-13 12:29:28 +0300

    change background

--- resources/views/campaign.blade.php
+++ resources/views/campaign.blade.php
@@ -3,7 +3,8 @@
 @section('pageClass', 'index')
 
 @section('content')
-    <header class="header--landing header--front-slide-1">
+    <header class="header--landing header--front-slide-1" @if(App::getLocale()=='nl') style="background: #fff url(/images/block-two-bg-nl.jpg) no-repeat center center; background-size: cover;
+    background-position: 60%;" @endif>
         <div class="header-nav promo-nav">
             <div class="container-fluid">
                     <div class="row">
