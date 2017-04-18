f9e8c478d6a9aa0f3df6ceea41477d2d8f871473
Max <adm@Mac-Pro-Aleksandr.local>
2017-04-18 17:00:49 +0300

    add new date

--- resources/views/admin/customers/home.blade.php
+++ resources/views/admin/customers/home.blade.php
@@ -29,7 +29,7 @@
 						<td>{{ \Jenssegers\Date\Date::createFromFormat('Y-m-d H:i:s', $customer->created_at)->format('j. M Y H:i') }}</td>
 						<td>
 							@if($customer->plan->getSubscriptionCancelledAt())
-							{{ \Date::createFromFormat('Y-m-d H:i:s', $customer->plan->getSubscriptionCancelledAt())->format('Y/m/d') }}
+							{{ \Date::createFromFormat('Y-m-d H:i:s', $customer->plan->getSubscriptionCancelledAt())->format('Y/m/d H:i') }}
 							@else
 								No
 							@endif
