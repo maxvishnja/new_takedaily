2a0c4ee717e44d55a7c171a2c254312172483143
Max <adm@Mac-Pro-Aleksandr.local>
2017-05-25 15:01:52 +0300

    add feedback

--- app/Console/Commands/SendHealthMail.php
+++ app/Console/Commands/SendHealthMail.php
@@ -43,28 +43,31 @@ class SendHealthMail extends Command
 
         $repo = new CustomerRepository();
         $customers = $repo->getNewCustomer();
-        foreach ( $customers as $customer ) {
 
-            \App::setLocale($customer->getLocale());
+            foreach ( $customers as $customer ) {
+            if($customer){
+                \App::setLocale($customer->getLocale());
 
-            $mailEmail = $customer->getEmail();
-            $mailName  = $customer->getName();
-            if($customer->getLocale()== 'nl') {
-                $fromEmail = 'info@takedaily.nl';
-            } else{
-                $fromEmail = 'info@takedaily.dk';
-            }
+                $mailEmail = $customer->getEmail();
+                $mailName  = $customer->getName();
+                if($customer->getLocale()== 'nl') {
+                    $fromEmail = 'info@takedaily.nl';
+                } else{
+                    $fromEmail = 'info@takedaily.dk';
+                }
 
-            \Log::info('Health mail send to Customer ID '.$customer->id." email ".$mailEmail);
+                \Log::info('Health mail send to Customer ID '.$customer->id." email ".$mailEmail);
 
-            \Mail::queue( 'emails.control-health', [ 'locale' => $customer->getLocale(), 'name' => $customer->getFirstname(), 'id' =>$customer->id ], function ( Message $message ) use ( $mailEmail, $mailName, $fromEmail )
-            {
-                $message->from( $fromEmail, 'TakeDaily' );
-                $message->to( $mailEmail, $mailName );
-                $message->subject( trans( 'mails.control-health.title' ) );
-            } );
+                \Mail::queue( 'emails.control-health', [ 'locale' => $customer->getLocale(), 'name' => $customer->getFirstname(), 'id' =>$customer->id ], function ( Message $message ) use ( $mailEmail, $mailName, $fromEmail )
+                {
+                    $message->from( $fromEmail, 'TakeDaily' );
+                    $message->to( $mailEmail, $mailName );
+                    $message->subject( trans( 'mails.control-health.title' ) );
+                } );
                 continue;
+                }
+            }
         }
 
-    }
+
 }
