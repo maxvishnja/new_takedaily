7e4a253cde738dc1b9d4e6e94dc922e71bb89b0d
Max <adm@Mac-Pro-Aleksandr.local>
2017-06-20 17:43:13 +0300

    add rules

--- resources/views/admin/customers/home.blade.php
+++ resources/views/admin/customers/home.blade.php
@@ -33,7 +33,7 @@
 							{{ \Date::createFromFormat('Y-m-d H:i:s', $customer->created_at)->format('Y/m/d H:i')}}
 						</td>
 						<td>
-							{{ $customer->plan->getSubscriptionCancelledAt() }}
+							{{ $customer->plan }}
 							{{--@if(!is_null($customer->plan->getSubscriptionCancelledAt()))--}}
 							{{--{{ \Date::createFromFormat('Y-m-d H:i:s', $customer->plan->getSubscriptionCancelledAt())->format('Y/m/d H:i') }}--}}
 							{{--@else--}}
