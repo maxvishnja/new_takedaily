9d73104aa3bea2389eb96fb2dac2e4e947c3d6fc
Max <adm@Mac-Pro-Aleksandr.local>
2017-09-14 11:45:57 +0300

    fix bug in Stat

--- app/Http/Controllers/Dashboard/StatsController.php
+++ app/Http/Controllers/Dashboard/StatsController.php
@@ -107,7 +107,9 @@ class StatsController extends Controller
                     $email_array[$i]['Signupdate'] = \Date::createFromFormat('Y-m-d H:i:s', $customer->created_at)->format('d-m-Y');
                     if ($customer->plan->subscription_rebill_at != null){
                         $email_array[$i]['Nextpayment'] = \Date::createFromFormat('Y-m-d H:i:s', $customer->plan->subscription_rebill_at)->format('d-m-Y');
-                     }
+                     } else{
+                        $email_array[$i]['Nextpayment'] = '';
+                    }
                     $email_array[$i]['Voucher'] = $customer->plan->getLastCoupon();
                     $email_array[$i]['Amount'] = $customer->order_count;
                     $email_array[$i]['Source'] = '';
