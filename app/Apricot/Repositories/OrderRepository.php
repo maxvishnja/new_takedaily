9f8c610cb2194c6c4b29c664d0401284056f5762
Max <administrator@iMac-Administrator.local>
2017-11-23 14:53:07 +0300

    change graph

--- app/Apricot/Repositories/OrderRepository.php
+++ app/Apricot/Repositories/OrderRepository.php
@@ -87,7 +87,6 @@ class OrderRepository
     {
         return Order::selectRaw("YEAR(created_at) as year, MONTH(created_at) as month, COUNT(DISTINCT id) as total")
             ->groupBy(\DB::raw("YEAR(created_at), MONTH(created_at)"))
-            ->whereNull('repeat')
             ->get();
     }
 
@@ -103,9 +102,9 @@ class OrderRepository
 
     public static function getPaidOrder($year, $month){
 
-	    $ordersDK = Order::where('currency', '=', 'DKK')->whereYear('created_at', '=', $year)->whereMonth('created_at', '=', $month)->sum('total');
+	    $ordersDK = Order::where('currency', '=', 'DKK')->whereYear('created_at', '=', $year)->whereMonth('created_at', '=', $month)->whereNull('repeat')->sum('total');
 
-	    $ordersEUR = Order::where('currency', '=', 'EUR')->whereYear('created_at', '=', $year)->whereMonth('created_at', '=', $month)->sum('total');
+	    $ordersEUR = Order::where('currency', '=', 'EUR')->whereYear('created_at', '=', $year)->whereMonth('created_at', '=', $month)->whereNull('repeat')->sum('total');
 
 
 	    $sumEUR = number_format($ordersEUR * 7.45 / 100, 2, '.', '');
