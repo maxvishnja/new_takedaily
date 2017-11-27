5bce4fe1c4edbf695c894ec1d15b2ea35772906f
Milos Diklic <sholeem@gmail.com>
2017-11-27 15:28:43 +0100

    MC resolving on cherry picking

--- resources/views/includes/app/footer.blade.php
+++ resources/views/includes/app/footer.blade.php
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-md-4 footer_column footer_column--newsletters text-center">
                     <h3 class="footer_title">{{ trans('footer.columns.one.title') }}</h3>
-                    <form method="post" action="{{ url()->action('MailchimpEmailSignup@post') }}" class="m-t-20 m-b-10"
-                          id="mailchimp_signup">
+                    <form method="post" action="https://client3.mailmailmail.net/form.php?form=656" id="frmSS656"
+                          onsubmit="return CheckForm656(this);" class="m-t-20 m-b-10">
                         <div class="row">
                             <div class="col-sm-8">
                                 <input type="email" name="email" id="input_newsletters_email"
@@ -85,10 +85,10 @@
         </div>
     </div>
 </footer>
-        @if(Auth::user() && Auth::user()->isUser() && Config::get('app.debug') == 1)
-            @include('includes.modal')
-        @endif
-        <!-- Yandex.Metrika counter -->
+@if(Auth::user() && Auth::user()->isUser() && Config::get('app.debug') == 1)
+    @include('includes.modal')
+@endif
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript">
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function () {
@@ -105,10 +105,10 @@
         });
 
         var n = d.getElementsByTagName("script")[0],
-                s = d.createElement("script"),
-                f = function () {
-                    n.parentNode.insertBefore(s, n);
-                };
+            s = d.createElement("script"),
+            f = function () {
+                n.parentNode.insertBefore(s, n);
+            };
         s.type = "text/javascript";
         s.async = true;
         s.src = "https://mc.yandex.ru/metrika/watch.js";
@@ -167,120 +167,120 @@
 
 @yield('footer_scripts')
 @if(Auth::user() && Auth::user()->isUser())
-<script>
-    $(function () {
-        $('#twitters').on('keyup', function () {
-            $newLength = 110 - $(this).val().length;
-            if($(this).val().length > 110){
-                $(this).val($(this).val().substring(0, (110)));
-            } else{
-                $('.twit-count').html($newLength);
-            }
-
-
-        });
-        $('.sharedEmail').on('submit', function(e){
-            e.preventDefault();
-
-            $.ajax({
-                type: 'POST',
-                data: $('form.sharedEmail').serialize(),
-                url: '{{ route("shared-email") }}',
-                success: function (data) {
-                    $('.tab-pane').removeClass('active');
-                    $('.nav-tabs li').removeClass('active');
-                    $('#thanks').addClass('active');
-
+    <script>
+        $(function () {
+            $('#twitters').on('keyup', function () {
+                $newLength = 110 - $(this).val().length;
+                if($(this).val().length > 110){
+                    $(this).val($(this).val().substring(0, (110)));
+                } else{
+                    $('.twit-count').html($newLength);
                 }
 
+
             });
+            $('.sharedEmail').on('submit', function(e){
+                e.preventDefault();
+
+                $.ajax({
+                    type: 'POST',
+                    data: $('form.sharedEmail').serialize(),
+                    url: '{{ route("shared-email") }}',
+                    success: function (data) {
+                        $('.tab-pane').removeClass('active');
+                        $('.nav-tabs li').removeClass('active');
+                        $('#thanks').addClass('active');
+
+                    }
+
+                });
 
 
-        });
+            });
 
             window.fbAsyncInit = function() {
-            // init the FB JS SDK
-            FB.init({
-                appId: '{{ env('FACEBOOK_APP_ID') }}',
-                xfbml: true,
-                status     : true
-            });
+                // init the FB JS SDK
+                FB.init({
+                    appId: '{{ env('FACEBOOK_APP_ID') }}',
+                    xfbml: true,
+                    status     : true
+                });
 
-        };
+            };
 
             // Load the SDK asynchronously
             (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/all.js";
-            fjs.parentNode.insertBefore(js, fjs);
-             }(document, 'script', 'facebook-jssdk'));
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/all.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
 
             function FBShareOp(){
-            var description	   = $('#myFbMes').val() + '\r\n' +
-                                 $('.fbMes').html();
-            var share_url	   =	'{!! \App\Apricot\Helpers\ShareLink::get(Auth::user()->id) !!}';
-            FB.ui({
-            method: 'share',
-            mobile_iframe: true,
-            href: share_url,
-            quote: description
+                var description	   = $('#myFbMes').val() + '\r\n' +
+                    $('.fbMes').html();
+                var share_url	   =	'{!! \App\Apricot\Helpers\ShareLink::get(Auth::user()->id) !!}';
+                FB.ui({
+                    method: 'share',
+                    mobile_iframe: true,
+                    href: share_url,
+                    quote: description
 
-        }, function(response) {
-            if(response){
-                $('.tab-pane').removeClass('active');
-                $('.nav-tabs li').removeClass('active');
-                $('#thanks').addClass('active');
+                }, function(response) {
+                    if(response){
+                        $('.tab-pane').removeClass('active');
+                        $('.nav-tabs li').removeClass('active');
+                        $('#thanks').addClass('active');
+
+                    }
+                    else{}
+                });
 
             }
-            else{}
-        });
 
-        }
+            $(document).on('click', '.shareFb', function(){
+                FBShareOp();
 
-        $(document).on('click', '.shareFb', function(){
-            FBShareOp();
+            });
 
-        });
+            var popupSize = {
+                width: 580,
+                height: 360
+            };
 
-        var popupSize = {
-            width: 580,
-            height: 360
-        };
+            $(document).on('click', '.shareTw', function(e){
 
-        $(document).on('click', '.shareTw', function(e){
+                e.preventDefault();
 
-            e.preventDefault();
-
-            var
+                var
                     verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
                     horisontalPos = Math.floor(($(window).height() - popupSize.height) / 2);
-            var txt = $('#twitters').val();
-            var url = "https://twitter.com/intent/tweet?text=" + txt.replace(/#/g,'') + "&url={{ \App\Apricot\Helpers\ShareLink::get(Auth::user()->id) }}";
+                var txt = $('#twitters').val();
+                var url = "https://twitter.com/intent/tweet?text=" + txt.replace(/#/g,'') + "&url={{ \App\Apricot\Helpers\ShareLink::get(Auth::user()->id) }}";
 
-            var popup = window.open(url, 'twitter',
+                var popup = window.open(url, 'twitter',
                     'width='+popupSize.width+',height='+popupSize.height+
                     ',left='+verticalPos+',top='+horisontalPos+
                     ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
 
-            if (popup) {
-                popup.focus();
-                e.preventDefault();
+                if (popup) {
+                    popup.focus();
+                    e.preventDefault();
 
-            }
+                }
 
-            setTimeout(function() {
-                $('.tab-pane').removeClass('active');
-                $('.nav-tabs li').removeClass('active');
-                $('#thanks').addClass('active');
-            }, 2000);
+                setTimeout(function() {
+                    $('.tab-pane').removeClass('active');
+                    $('.nav-tabs li').removeClass('active');
+                    $('#thanks').addClass('active');
+                }, 2000);
+
+            });
+
 
         });
-
-
-    });
-</script>
+    </script>
 @endif
 @if(isset($errors))
     @if($errors->has())
@@ -365,70 +365,101 @@
             });
         });
     </script>
-    @endif
+@endif
 
-            <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-        (function () {
-            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/{{ trans('general.tawk_id') }}/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/{{ trans('general.tawk_id') }}/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 
-    <script>
+<script>
 
-        $(window).scroll(function () {
-            var hgt = $('.header-nav').height();
-            var top = $(document).scrollTop();
-            if (top > 2) $(".header-nav").css({position: 'fixed'});
-            else $(".header-nav").css({position: 'relative'});
-        });
+    $(window).scroll(function () {
+        var hgt = $('.header-nav').height();
+        var top = $(document).scrollTop();
+        if (top > 2) $(".header-nav").css({position: 'fixed'});
+        else $(".header-nav").css({position: 'relative'});
+    });
+
+    // <![CDATA[
+
+    function CheckMultiple656(frm, name) {
+        for (var i=0; i < frm.length; i++)
+        {
+            fldObj = frm.elements[i];
+            fldId = fldObj.id;
+            if (fldId) {
+                var fieldnamecheck=fldObj.id.indexOf(name);
+                if (fieldnamecheck != -1) {
+                    if (fldObj.checked) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    function CheckForm656(f) {
+        var email_re = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+        if (!email_re.test(f.email.value)) {
+            alert("Venligst indtaste din e-mail-adresse.:");
+            f.email.focus();
+            return false;
+        }
+
+        return true;
+    }
+
+    // ]]>
 
 
 
 
 
+    $("#mailchimp_signup").submit(function (e) {
+        e.preventDefault();
 
+        var $form = $(this);
 
-        $("#mailchimp_signup").submit(function (e) {
-            e.preventDefault();
+        $.post($form.attr('action'), $form.serialize()).success(function (response) {
 
-            var $form = $(this);
-
-            $.post($form.attr('action'), $form.serialize()).success(function (response) {
-                console.log(response);
-                swal({
-                    title: "{{ trans('message.success-title') }}",
-                    text: "{{ trans('mailchimp.thanks') }}",
-                    type: "success",
-                    html: true,
-                    allowOutsideClick: true,
-                    confirmButtonText: "{{ trans('popup.button-close') }}",
-                    confirmButtonColor: "#3AAC87",
-                    timer: 6000
-                });
-            }).error(function (response) {
-                swal({
-                    title: "{{ trans('message.error-title') }}",
-                    text: response.responseJSON.errors,
-                    type: "error",
-                    html: true,
-                    allowOutsideClick: true,
-                    confirmButtonText: "{{ trans('popup.button-close') }}",
-                    confirmButtonColor: "#3AAC87",
-                    timer: 6000
-                });
+            swal({
+                title: "{{ trans('message.success-title') }}",
+                text: "{{ trans('mailchimp.thanks') }}",
+                type: "success",
+                html: true,
+                allowOutsideClick: true,
+                confirmButtonText: "{{ trans('popup.button-close') }}",
+                confirmButtonColor: "#3AAC87",
+                timer: 6000
+            });
+        }).error(function (response) {
+            swal({
+                title: "{{ trans('message.error-title') }}",
+                text: response.responseJSON.errors,
+                type: "error",
+                html: true,
+                allowOutsideClick: true,
+                confirmButtonText: "{{ trans('popup.button-close') }}",
+                confirmButtonColor: "#3AAC87",
+                timer: 6000
             });
         });
-    </script>
-    @if(App::environment() != 'local')
-        @yield('tracking-scripts')
-    @endif
-    </body>
-    </html>
+    });
+</script>
+@if(App::environment() != 'local')
+@yield('tracking-scripts')
+@endif
+</body>
+</html>
