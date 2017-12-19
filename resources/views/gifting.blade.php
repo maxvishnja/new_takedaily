0c88b290b9449cab1ceb86b9e8337bf295ae0342
Milos Diklic <sholeem@gmail.com>
2017-12-19 11:30:45 +0100

    gifting page fix

--- resources/views/gifting.blade.php
+++ resources/views/gifting.blade.php
@@ -51,7 +51,7 @@
 			</form>
 		</div>
 
-		<div class="gifting-steps">
+		<div class="row">
 			<div class="col-lg-4 col-md-4 col-sm-4 text-center">
 				<img src="/images/gifting/step_1.png"
 					 alt="{{ trans('gifting.steps.one.title') }}">
