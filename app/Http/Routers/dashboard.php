ed448be003bd54df5737432b71b78b21933c0de6
Max <administrator@iMac-Administrator.local>
2017-12-22 13:22:50 +0300

    add button to order

--- app/Http/Routers/dashboard.php
+++ app/Http/Routers/dashboard.php
@@ -92,12 +92,13 @@ Route::group( [ 'prefix' => 'dashboard', 'middleware' => 'admin' ], function ()
 	Route::get( 'almost/send/{id}', 'Dashboard\AlmostCustomersController@sendOne' );
 	Route::get( 'almost/csv', 'Dashboard\AlmostCustomersController@getCsv' );
 	Route::get( 'rewrites/remove/{id}', 'Dashboard\RewriteController@remove' );
-
+    Route::get( 'orders/createNl', 'Dashboard\OrderController@createCsvNl' );
+    Route::get( 'orders/createDk', 'Dashboard\OrderController@createCsvDk' );
 	Route::resource( 'orders', 'Dashboard\OrderController' );
 	Route::get( 'orders/mark-sent/{id}', 'Dashboard\OrderController@markSent' );
 	Route::get( 'orders/refund/{id}', 'Dashboard\OrderController@refund' );
 	Route::get( 'orders/download/{id}', 'Dashboard\OrderController@download' );
-	Route::get( 'orders/create', 'Dashboard\OrderController@createCsv' );
+
 	Route::get( 'orders/download-sticker/{id}', 'Dashboard\OrderController@downloadSticker' );
 	Route::resource( 'coupons', 'Dashboard\CouponController' );
 	Route::resource( 'settings', 'Dashboard\SettingController' );
