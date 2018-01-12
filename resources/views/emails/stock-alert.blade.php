77951e7350fbc94c6eea8bd3a4b23b90f8bd2354
Milos Diklic <sholeem@gmail.com>
2018-01-12 13:44:23 +0100

    update and fix stock

--- resources/views/emails/stock-alert.blade.php
+++ resources/views/emails/stock-alert.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <p class="size-16" style='font-style: normal;font-weight: 400;Margin-bottom: 0;Margin-top: 16px;font-size: 16px;line-height: 24px;font-family: "Open Sans",sans-serif;color: #60666d;text-align: center;'>
-        {!! trans('mails.subscription.content') !!}
+        Hello. Stock item {{ $item->name }} is getting low. <br>
+        Please check stock dashboard.
     </p>
 @endsection
