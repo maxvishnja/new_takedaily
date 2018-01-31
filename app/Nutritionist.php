97a25f1b3d09ea293f9df44ef206efdb2c4f6da2
Max <administrator@iMac-Administrator.local>
2018-01-31 17:43:23 +0300

    add filed desc to nutritionist

--- app/Nutritionist.php
+++ app/Nutritionist.php
@@ -24,7 +24,7 @@ class Nutritionist extends Model
      * @var array
      */
 
-    protected $fillable = ['id', 'first_name', 'last_name', 'title', 'image', 'email', 'locale', 'active', 'order'];
+    protected $fillable = ['id', 'first_name', 'last_name', 'title', 'image', 'email', 'locale', 'active', 'order', 'desc'];
 
     /**
      * The attributes excluded from the model's JSON form.
