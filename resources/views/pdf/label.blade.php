4d2e07b47d65417223633ccb80e497e0b7a99896
Milos Diklic <sholeem@gmail.com>
2018-02-01 19:18:42 +0100

    revert change label

--- resources/views/pdf/label.blade.php
+++ resources/views/pdf/label.blade.php
@@ -1,27 +1,27 @@
 <?php App::setLocale($customer->getLocale()); ?>
 
 @if($customer->getLocale() == 'nl')
-<div style="position:relative;  width: 170mm;">
+	<div style="position:relative;  width: 170mm;">
 
 
 		<img style="width: 112pt; height: 59pt; position: absolute; top: 0; right: 25pt;" src="{{ asset('/images/logo-postnl@2x.png') }}"/>
 		<span style="position: absolute; top: 26mm; right: 25pt; font-size: 9pt; font-weight: 200; color: #1A8562;">#{{ $order->getPaddedId() }}</span>
 
 
-	<address>
-		<img style="width: 100pt; height: 19pt; margin-bottom: 1pt; " src="{{ asset('/images/pdf-logo.png') }}"/>
-		<div style="margin-bottom: 4pt; font-size:9pt"><strong>{{ $customer->getName() }}</strong>@if($customer->getCustomerAttribute('company') != '') @if($customer->getLocale() == 'da'), C/o @else - @endif {{ $customer->getCustomerAttribute('company') }}@endif</div>
-		<p style="font-size:10pt">{{ $customer->getCustomerAttribute('address_line1') }}, {{ $customer->getCustomerAttribute('address_number') }}<br/>
-			@if($customer->getCustomerAttribute('address_line2') != '')
-				{{ $customer->getCustomerAttribute('address_line2') }}<br/>
-			@endif
-			{{ $customer->getCustomerAttribute('address_postal') }}&nbsp;&nbsp;{{ $customer->getCustomerAttribute('address_city') }}<br/>
-			{{ ucfirst($customer->getCustomerAttribute('address_country')) }}</p>
-	</address>
+		<address>
+			<img style="width: 100pt; height: 19pt; margin-bottom: 1pt; " src="{{ asset('/images/pdf-logo.png') }}"/>
+			<div style="margin-bottom: 4pt; font-size:9pt"><strong>{{ $customer->getName() }}</strong>@if($customer->getCustomerAttribute('company') != '') @if($customer->getLocale() == 'da'), C/o @else - @endif {{ $customer->getCustomerAttribute('company') }}@endif</div>
+			<p style="font-size:10pt">{{ $customer->getCustomerAttribute('address_line1') }}, {{ $customer->getCustomerAttribute('address_number') }}<br/>
+				@if($customer->getCustomerAttribute('address_line2') != '')
+					{{ $customer->getCustomerAttribute('address_line2') }}<br/>
+				@endif
+				{{ $customer->getCustomerAttribute('address_postal') }}&nbsp;&nbsp;{{ $customer->getCustomerAttribute('address_city') }}<br/>
+				{{ ucfirst($customer->getCustomerAttribute('address_country')) }}</p>
+		</address>
 
-	<div style="font-size: 8pt; font-weight: 200; margin-left:5pt"><small>{{ trans('label-product.return') }}</small></div>
-</div>
-	@else
+		<div style="font-size: 8pt; font-weight: 200; margin-left:5pt"><small>{{ trans('label-product.return') }}</small></div>
+	</div>
+@else
 
 	<div style="position:relative;  width: 170mm;">
 
@@ -44,8 +44,8 @@
 			<img style="width: 112pt; height: 59pt; position: absolute; top: 0; right: 25pt;" src="{{ asset('/images/logo-postnl@2x.png') }}"/>
 			<span style="position: absolute; top: 26mm; right: 25pt; font-size: 9pt; font-weight: 200; color: #1A8562;">#{{ $order->getPaddedId() }}</span>
 		@endif
-			<span style="font-size: 8pt; position: absolute; top: -7mm; left: -5pt; font-weight: 200; ">#{{ $order->getPaddedId() }}</span>
-			<br/>
+		<span style="font-size: 8pt; position: absolute; top: -7mm; left: -5pt; font-weight: 200; ">#{{ $order->getPaddedId() }}</span>
+		<br/>
 		<address>
 
 			<p style="font-size: 8pt; margin-bottom: 1pt;  ">CODE: {{ $order->labelTekst3 }}</p>
@@ -66,4 +66,4 @@
 
 
 
-	@endif
+@endif
